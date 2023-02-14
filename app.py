d7ea000fd2d5c61cba08a8d963018e2c79038cbd
tjmisko <tjmisko@users.noreply.github.com>
2023-02-14 13:21:37 -0800

    implemented user login system for sspi page

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+from sqlalchemy_serializer import SerializerMixin
+from dataclasses import dataclass
+from json import JSONEncoder
 # load in the Flask class from the flask library
 from flask import Flask, render_template, url_for, redirect
 # load in the SQLAlchemy object to handle setting up the user data database
 from flask_sqlalchemy import SQLAlchemy
 # load in the UserMixin to handle the creation of user objects (not strictly necessary
 # but it's a nice automation so we don't have to think too much about it)
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 # load in the packages that make the forms pretty for submitting login and
 # and restration data
 from flask_wtf import FlaskForm
@@ -16,17 +19,27 @@ from flask_bcrypt import Bcrypt
 
 # create a Flask object
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/tristanmisko/Documents/Projects/sspi-data-collection/instance/database.db'
 app.config['SECRET_KEY'] = 'thisneedstobechanged'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 # create a User object that can track the id, username, and password of SSPI
 # team members in the database database.db
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+@dataclass
+class User(db.Model, UserMixin, SerializerMixin):
+    id:int = db.Column(db.Integer, primary_key=True)
+    username:str = db.Column(db.String(20), nullable=False, unique=True)
+    password:str = db.Column(db.String(80), nullable=False)
+
 
 # create a registration form for new users
 class RegisterForm(FlaskForm):
@@ -57,10 +70,26 @@ class LoginForm(FlaskForm):
 def home():
     return render_template('home.html')
 
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(username=login_form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, login_form.password.data):
+            login_user(user)
+            return redirect(url_for('dashboard'))
     return render_template('login.html', form=login_form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -74,9 +103,11 @@ def register():
 
     return render_template('register.html', form=register_form)
 
-@app.route('/alternate')
-def alternate():
-    return "This is a different page!"
+@app.route('/check-user-db')
+@login_required
+def get_all_users():
+    users = User.query.all()
+    return str(users)
 
 # run the app
 # an little explanation of what's going on:
