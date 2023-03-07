cabaf6f50455fcbaf36977b8c7bc4a119bacc3c0
tjmisko <tjmisko@users.noreply.github.com>
2023-03-07 13:00:07 -0800

    created header bar for pages

--- flask_app/app.py
+++ flask_app/app.py
@@ -77,6 +77,13 @@ class LoginForm(FlaskForm):
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 @app.route('/dashboard')
 @login_required
 def dashboard():
