49aadab9870668cf7eea3ad1d027d28487636fa0
tjmisko <tjmisko@users.noreply.github.com>
2023-03-10 11:39:52 -0800

    used pipreqs and pip-tools to rebuild the requirements file and add subdependencies

--- app.py
+++ app.py
@@ -23,20 +23,13 @@ from bson import json_util
 import requests
 from datetime import datetime
 
-def create_app():
-    app = Flask(__name__)
-    # SQLAlchemy configuration for administrator login
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/tristanmisko/Documents/Projects/sspi-data-collection/flask_app/instance/database.db'
-    app.config['SECRET_KEY'] = 'thisneedstobechanged'
-    db = SQLAlchemy(app)
-    bcrypt = Bcrypt(app)
-    # MongoDB configuration
-    client = MongoClient('localhost', 27017)
-    sspidb = client.flask_db
-    sspi_main_data = sspidb.sspi_main_data
-    return app
 
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
+client = MongoClient('localhost', 27017)
+sspidb = client.flask_db
+sspi_main_data = sspidb.sspi_main_data
 
 login_manager = LoginManager()
 login_manager.init_app(app)
