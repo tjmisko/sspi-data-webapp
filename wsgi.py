6d60f7514dde003c17de8419495923f028760923
tjmisko <tjmisko@users.noreply.github.com>
2023-03-13 14:44:24 -0700

    updated assignment three

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from sspi_flask_app import init_app
 from config import Config, DevConfig, ProdConfig
 
-app = init_app()
+app = init_app(DevConfig)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
