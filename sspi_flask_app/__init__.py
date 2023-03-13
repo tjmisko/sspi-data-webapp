093afba2a5a5a0fe9ca76bb9ddbceda3abf720cd
tjmisko <tjmisko@users.noreply.github.com>
2023-03-13 11:24:48 -0700

    initialized usermodel in models module

--- sspi_flask_app/__init__.py
+++ sspi_flask_app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import MongoClient, PyMongo
 from config import Config, DevConfig, ProdConfig
@@ -14,12 +15,17 @@ def init_app():
     app = Flask(__name__)
     app.config.from_object(DevConfig)
     print("DatabaseURI:" + DevConfig.SQLALCHEMY_DATABASE_URI)
-    # Initialize Plugins
+    # Initialize SQLAlchemy Database
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
     
     with app.app_context():
         from .home import routes
-
+        from .auth import auth
         # Register Blueprints
+        db.create_all()
         app.register_blueprint(routes.home_bp)
+        app.register_blueprint(auth.auth_bp)
         return app
