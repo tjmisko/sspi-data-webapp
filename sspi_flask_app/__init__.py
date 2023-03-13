19fcdc5e4a6e59dbd3c8421bfc14e6f8472570aa
tjmisko <tjmisko@users.noreply.github.com>
2023-03-13 14:44:24 -0700

    updated assignment three

--- sspi_flask_app/__init__.py
+++ sspi_flask_app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import MongoClient, PyMongo
-from config import Config, DevConfig, ProdConfig
 from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
@@ -13,27 +12,26 @@ client = MongoClient('localhost', 27017)
 sspidb = client.flask_db
 sspi_main_data = sspidb.sspi_main_data
 
-def init_app():
+def init_app(Config):
     # Initialize Core application
     app = Flask(__name__)
-    app.config.from_object(DevConfig)
-    print("DatabaseURI:" + DevConfig.SQLALCHEMY_DATABASE_URI)
+    app.config.from_object(Config)
+    print("DatabaseURI:" + Config.SQLALCHEMY_DATABASE_URI)
     # Initialize SQLAlchemy Database
     db.init_app(app)
     # Initialize password encryption
     flask_bcrypt.init_app(app)
     # Initialize Login manager
     login_manager.init_app(app)
-    # initialize Bcrypt
-    bcrypt = Bcrypt(app)
 
     with app.app_context():
+        # read in the appropriate modules
         from .home import routes
         from .auth import auth
-        from .models import usermodel
         from .api import datatest
-        # Register Blueprints
+        # Register database
         db.create_all()
+        # Register Blueprints
         app.register_blueprint(datatest.datatest_bp)
         app.register_blueprint(routes.home_bp)
         app.register_blueprint(auth.auth_bp)
