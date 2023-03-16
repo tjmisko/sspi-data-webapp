48f0ee0511fcf5a39869cefd033e817e18c951de
tjmisko <tjmisko@users.noreply.github.com>
2023-03-16 12:44:20 -0700

    began documenting the sdg collection functions

--- sspi_flask_app/api/source_utilities/sdg.py
+++ sspi_flask_app/api/source_utilities/sdg.py
@@ -3,6 +3,10 @@ import requests
 from ..api import parse_json
 
 def collectAvailableGeoAreas(indicator_code):
+    """
+    To collect the data, we need to know which countries it is available for
+
+    """
     json_data = requests.get("https://unstats.un.org/sdgapi/v1/sdg/Indicator/" + indicator_code + "/GeoAreas").json()
     m49_list = []
     for observation in json_data:
@@ -10,8 +14,13 @@ def collectAvailableGeoAreas(indicator_code):
     return m49_list
 
 def collectSDGIndicatorData(indicator_code):
+    """
+    This function collects the data from the SDG database
+
+    """
     base_url = "https://unstats.un.org/sdgapi/v1/sdg/Indicator/Data?indicator=" + indicator_code
     m49_list = collectAvailableGeoAreas(indicator_code)
+    big_json = {}
     for year in range(2000,2023):
         slug = "&timePeriod=" + str(year)
         for country in m49_list:
