b0d412574ed6b03d1c1c441f47b1e1f870f6c5e7
tjmisko <tjmisko@users.noreply.github.com>
2023-03-28 13:16:40 -0700

    implemented country lookup route

--- sspi_flask_app/api/sspi/sustainability/ecosystem/redlist.py
+++ sspi_flask_app/api/sspi/sustainability/ecosystem/redlist.py
@@ -1,7 +1,8 @@
 from ....source_utilities.sdg import collectSDGIndicatorData
+from ....api import api_bp
 
-
-def collect():
+@api_bp.route("/collect/biodiversity")
+def collect_biodiversity():
     data = collectSDGIndicatorData("15.5.1")
     return str(type(data))
 
