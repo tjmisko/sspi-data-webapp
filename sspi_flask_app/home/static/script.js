9b3f42c3ceae27bc804ea00e2f079455627a320a
tjmisko <tjmisko@users.noreply.github.com>
2023-03-31 13:42:41 -0700

    created placeholder chart and deferred script.js running until page load

--- sspi_flask_app/home/static/script.js
+++ sspi_flask_app/home/static/script.js
@@ -1,1 +1,1 @@
-console.log("makeGraph.js loaded with chart.js version: "+Chart.version);
+console.log("makeGraph.js loaded with chart.js version: "+Chart.version);const ctx=document.getElementById('myChart');new Chart(ctx,{type:'bar',data:{labels:['Red','Blue','Yellow','Green','Purple','Orange'],datasets:[{label:'# of Votes',data:[12,19,3,5,2,3],borderWidth:1}]},options:{scales:{y:{beginAtZero:true}}}});
