d47cd8fd0fa76cd959643e52445e22513087b516
tjmisko <tjmisko@users.noreply.github.com>
2023-03-31 13:31:52 -0700

    added test chart to makeGraph

--- sspi_flask_app/home/static/js/makeGraph.js
+++ sspi_flask_app/home/static/js/makeGraph.js
@@ -1,25 +1,24 @@
 console.log("makeGraph.js loaded with chart.js version: " + Chart.version);
 
-<script>
-  const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
+new Chart(ctx, {
+type: 'bar',
+data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+    label: '# of Votes',
+    data: [12, 19, 3, 5, 2, 3],
+    borderWidth: 1
+    }]
+},
+options: {
+    scales: {
+    y: {
+        beginAtZero: true
     }
-  });
-</script>
+    }
+}
+});
+
  
