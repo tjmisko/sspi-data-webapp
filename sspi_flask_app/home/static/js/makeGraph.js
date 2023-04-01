fbca41c8dc37722ab46ad73a03f16486222d73eb
tjmisko <tjmisko@users.noreply.github.com>
2023-04-01 01:35:11 -0700

    implemented basic charting with data and front end calls

--- sspi_flask_app/home/static/js/makeGraph.js
+++ sspi_flask_app/home/static/js/makeGraph.js
@@ -1,23 +1,26 @@
 console.log("makeGraph.js loaded with chart.js version: " + Chart.version);
-
 const ctx = document.getElementById('myChart');
 
-new Chart(ctx, {
-type: 'bar',
-data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-    label: '# of Votes',
-    data: [12, 19, 3, 5, 2, 3],
-    borderWidth: 1
-    }]
-},
-options: {
-    scales: {
-    y: {
-        beginAtZero: true
+async function makeBarChart(IndicatorCode){
+    await fetch('/api/v1/query/' + IndicatorCode)
+        .then((response) => response.json())
+        .then((indicator_data) => 
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: indicator_data.map((observation) => (observation["Country"])),
+                    datasets: [{
+                    label: indicator_data[0]["IndicatorNameShort"],
+                    data: indicator_data.map((observation) => (observation["SCORE"])),
+                    borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                    }
+                }
+            })).update();
     }
-    }
-}
-});
-
