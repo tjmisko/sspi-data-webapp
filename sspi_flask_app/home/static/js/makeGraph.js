789b8a08912c093aa8b87cb20dfde91bee1c73dd
tjmisko <tjmisko@users.noreply.github.com>
2023-04-01 02:03:35 -0700

    struggling to get chart updates to work

--- sspi_flask_app/home/static/js/makeGraph.js
+++ sspi_flask_app/home/static/js/makeGraph.js
@@ -1,26 +1,31 @@
 console.log("makeGraph.js loaded with chart.js version: " + Chart.version);
 const ctx = document.getElementById('myChart');
+const BarChart = new Chart(ctx, {});
 
 async function makeBarChart(IndicatorCode){
     await fetch('/api/v1/query/' + IndicatorCode)
         .then((response) => response.json())
-        .then((indicator_data) => 
-            new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: indicator_data.map((observation) => (observation["Country"])),
-                    datasets: [{
-                    label: indicator_data[0]["IndicatorNameShort"],
-                    data: indicator_data.map((observation) => (observation["SCORE"])),
-                    borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                    y: {
+        .then((indicator_data) => console.log(indicator_data))
+        .finally((indicator_data) => updateBarChart(indicator_data))
+    }
+
+function updateBarChart(indicator_data) {
+    BarChart.data = {
+            labels: indicator_data.map((observation) => (observation["Country"])),
+            datasets: [{
+            label: indicator_data[0]["IndicatorNameShort"],
+            data: indicator_data.map((observation) => (observation["SCORE"])),
+            borderWidth: 1
+            }]
+        }  
+    BarChart.options = {
+            scales: {
+                yAxes: [{
+                    ticks: {
                         beginAtZero: true
                     }
-                    }
-                }
-            })).update();
-    }
+                }]
+            }
+        }
+    BarChart.update();
+}
