9fcc83a362c313618c28585b6d154ab6e9836093
tjmisko <tjmisko@users.noreply.github.com>
2023-04-02 15:35:22 -0700

    handled bug in toggleRaw function that led to error

--- sspi_flask_app/home/static/script.js
+++ sspi_flask_app/home/static/script.js
@@ -3,10 +3,13 @@ async function makeBarChart(IndicatorCode){let response=await fetch('/api/v1/que
 let indicator_data=await response.json()
 indicator_data.sort((a,b)=>a.RAW-b.RAW)
 let y_axis=raw?getRaw(indicator_data):getScores(indicator_data)
-BarChart.data={labels:getCountries(indicator_data),datasets:[{label:indicator_data[0].IndicatorNameShort,data:y_axis,backgroundColor:'rgb(255, 99, 132)',borderColor:'rgb(255, 99, 132)',borderWidth:1}]}
-BarChart.options={scaleShowValues:true,layout:{padding:20},scales:{xAxes:[{ticks:{autoSkip:false}}]}},BarChart.update();}
-function toggleRaw(IndicatorCode){raw=!raw
-makeBarChart(IndicatorCode);}
+BarChart.data={labels:getCountries(indicator_data),datasets:[{datacode:IndicatorCode,label:indicator_data[0].IndicatorNameShort,data:y_axis,backgroundColor:'rgb(255, 99, 132)',borderColor:'rgb(255, 99, 132)',borderWidth:1}]}
+BarChart.options={scaleShowValues:true,layout:{padding:20},scales:{xAxes:[{id:'x',type:'category',title:{display:true,text:'Country'},ticks:{autoskip:true,}}]}}
+BarChart.update();}
+function toggleRaw(){raw=!raw
+try{IndicatorCode=BarChart.data.datasets[0].datacode
+makeBarChart(IndicatorCode);}catch(TypeError){console.log("No Chart Loaded!\n",TypeError)
+IndicatorCode=null;}}
 function getCountries(indicator_data){return indicator_data.map(data=>data.Country)}
 function getScores(indicator_data){return indicator_data.map(data=>data.SCORE)}
 function getRaw(indicator_data){return indicator_data.map(data=>data.RAW)}
