4a95b18eaf3f59a831a50329bbce89116569463a
tjmisko <tjmisko@users.noreply.github.com>
2023-04-02 16:03:50 -0700

    slight adjustments to graph

--- sspi_flask_app/home/static/js/makeGraph.js
+++ sspi_flask_app/home/static/js/makeGraph.js
@@ -12,7 +12,7 @@ raw=false
 async function makeBarChart(IndicatorCode){
     let response = await fetch('/api/v1/query/' + IndicatorCode)
     let indicator_data = await response.json()
-    indicator_data.sort((a, b) => a.RAW - b.RAW)
+    indicator_data.sort((a, b) => b.RANK - a.RANK)
     let y_axis = raw ? getRaw(indicator_data) : getScores(indicator_data)
     BarChart.data = {
         labels: getCountries(indicator_data),
