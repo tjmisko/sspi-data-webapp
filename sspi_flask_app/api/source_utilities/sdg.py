2cc07ee57ff738fe11360cbd9de4e68ef1d701d8
tjmisko <tjmisko@users.noreply.github.com>
2023-04-10 16:16:43 -0700

    deleted old blueprint architecture

--- sspi_flask_app/api/source_utilities/sdg.py
+++ sspi_flask_app/api/source_utilities/sdg.py
@@ -1,6 +1,10 @@
 import json
 import time
 import requests
-from ..api import parse_json
 
 # Implement API Collection for https://unstats.un.org/sdgapi/v1/sdg/Indicator/PivotData?indicator=14.5.1
+def collectSDGIndicatorData(IndicatorCode):
+    url_source = "https://unstats.un.org/sdgapi/v1/sdg/Indicator/PivotData?indicator=" + IndicatorCode
+    """make API request"""
+    data = requests.get(url_source).json()
+    return data
