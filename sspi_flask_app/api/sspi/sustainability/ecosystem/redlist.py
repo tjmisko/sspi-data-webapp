3b9f622a5c5b558e2c67e43db434f4c14911701d
tjmisko <tjmisko@users.noreply.github.com>
2023-04-10 16:16:43 -0700

    deleted old blueprint architecture

--- sspi_flask_app/api/sspi/sustainability/ecosystem/redlist.py
+++
@@ -1,7 +0,0 @@
-from ....source_utilities.sdg import collectSDGIndicatorData
-from ....api import api_bp
-
-@api_bp.route("/collect/biodiversity")
-def collect_biodiversity():
-    data = collectSDGIndicatorData("15.5.1")
-    return str(type(data))
