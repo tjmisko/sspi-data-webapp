1db5153b1b4a32d2194640a90171d929c9455699
tjmisko <tjmisko@users.noreply.github.com>
2023-04-17 12:15:09 -0700

    added print statment for dev sqlalchuri

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@ from sspi_flask_app import init_app
 from config import Config, DevConfig, ProdConfig
 
 print("Startup info:", str(dir(DevConfig)))
+print("SQLALCHEMY_DATABASE_URI", DevConfig.SQLALCHEMY_DATABASE_URI)
 app = init_app(DevConfig)
 
 if __name__ == "__main__":
