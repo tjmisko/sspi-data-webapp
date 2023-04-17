f3a9118098d05e6c46145243ecb094da3c9930f7
root <root@sspi-web-server.sspi.world>
2023-04-17 19:29:49 +0000

    updated wsgi for app

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
 from sspi_flask_app import init_app
+import sys
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='/var/www/sspi.world/logs/sspi.world.log', format='%(asctime)s %(message)s')
+sys.path.insert(0, '/var/www/sspi.world')
+sys.path.insert(0, '/var/www/sspi.world/env/lib/python3.9/site-packages')
+
 from config import Config, DevConfig, ProdConfig
 
 print("Startup info:", str(dir(DevConfig)))
