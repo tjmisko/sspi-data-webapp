5fd75aeb45d7514a978f617b19622f519bc5f823
tjmisko <tjmisko@users.noreply.github.com>
2023-04-17 09:32:04 -0700

    Further computation route implementation for BIODIV

--- sspi_flask_app/api/sspi/compute.py
+++ sspi_flask_app/api/sspi/compute.py
@@ -6,11 +6,11 @@ compute_bp = Blueprint("compute_bp", __name__,
                        static_folder="static", 
                        url_prefix="/compute")
 
-def check_indicator(indicatorCode):
+def indicator_data_available(IndicatorCode):
     """
     Check if indicator is in database
     """
-    return bool(sspi_clean_api_data.find_one({"indicatorCode": indicatorCode}))
+    return bool(sspi_raw_api_data.find_one({"RawDataDestination": IndicatorCode}))
 
 @compute_bp.route("/BIODIV", methods=['GET'])
 def compute_biodiv():
@@ -19,7 +19,12 @@ def compute_biodiv():
     - If no collection route is implemented, return a page with a message
     - If collection route is implemented, return a page with a button to collect the data
     If indicator is in database, compute the indicator from the raw data
+    - Indicator computation: average of the three scores for percentage of biodiversity in
+    marine, freshwater, and terrestrial ecosystems
     """
-    if not check_indicator("BIODIV"):
-        return render_template("compute.html", indicatorCode="BIODIV")
+    if not indicator_data_available("BIODIV"):
+        return render_template("missingdata.html", IndicatorCode="BIODIV")
+    # Should I do my aggregation operations in MongoDB or in Python?
+    raw_data = sspi_raw_api_data.find({"RawDataDestination": "BIODIV"})
+
     
