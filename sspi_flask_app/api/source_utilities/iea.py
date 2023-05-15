50b15d04e3ef116f94a5c28c23d35617b2e22ce7
tjmisko <tjmisko@users.noreply.github.com>
2023-05-15 16:56:50 -0700

    updated store_raw_api_data to be importable to modularize storage of observations

--- sspi_flask_app/api/source_utilities/iea.py
+++ sspi_flask_app/api/source_utilities/iea.py
@@ -3,6 +3,7 @@ import requests
 from ... import sspi_raw_api_data
 from flask_login import current_user
 from flask import redirect, url_for
+from ..api import store_raw_api_data
 
 def collectIEAData(IndicatorCode, RawDataDestination):
     collection_time = datetime.now()
@@ -10,21 +11,6 @@ def collectIEAData(IndicatorCode, RawDataDestination):
     store_raw_api_data(response, collection_time, RawDataDestination)
     return redirect(url_for('home_bp.data'))
 
-def store_raw_api_data(response, collection_time, RawDataDestination):
-    """
-    Store the response from an API call in the database
-    """
-    try:
-        for r in response:
-            sspi_raw_api_data.insert_one(
-                {"collection-info": {"CollectedBy": current_user.username,
-                                    "RawDataDestination": RawDataDestination,
-                                    "CollectedAt": collection_time}, 
-                "observation": r}) 
-        return response
-    except Exception as e:
-        print("Error storing API data:", e)
-        return "Error storing API data"
 
 
 
