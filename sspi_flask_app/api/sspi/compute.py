2ddd881ab1f6938d9174c90bd75db4fda6a8db8c
tjmisko <tjmisko@users.noreply.github.com>
2023-06-12 16:24:39 +0200

    wrote country m49 utility function

--- sspi_flask_app/api/sspi/compute.py
+++ sspi_flask_app/api/sspi/compute.py
@@ -3,11 +3,25 @@ from flask import Blueprint, request, render_template
 from ... import sspi_clean_api_data, sspi_raw_api_data
 import json
 from bson import json_util
+from pycountry import countries
 
 def parse_json(data):
     return json.loads(json_util.dumps(data))
 def print_json(data):
     print(json.dumps(data, indent=4, sort_keys=True))
+def format_m49_as_string(input):
+    """
+    Utility function ensuring that all M49 data is correctly formatted as a
+    string of length 3 for use with the pycountry library
+    """
+    input = int(input)
+    if input >= 100:
+        return str(input) 
+    elif input >= 10:
+        return '0' + str(input)
+    else: 
+        return '00' + str(input)
+        
 
 compute_bp = Blueprint("compute_bp", __name__,
                        template_folder="templates", 
@@ -30,65 +44,41 @@ def compute_biodiv():
     - Indicator computation: average of the three scores for percentage of biodiversity in
     marine, freshwater, and terrestrial ecosystems
     """
-    def flatten(dict, keyname):
-        """
-        Takes in a nested dictionary of the form
-        {
-            "a":
-                {
-                    "b": v1
-                    "c": v2
-                }
-            "d":
-                {
-                    "e": v3
-                    "f": v4
-                }
-        }
-        and flattens it into a list of the form
-        [
-            {keyname: "a", "b": v1, "c": v2},
-            {keyname: "d", "e": v3, "f": v4}
-        ]
-        
-        """
-        newlist = []
-        for k in dict.keys():
-            for obs in dict[k]:
-                print(obs)
-                flat_obs = obs.copy()
-                flat_obs[keyname] = k
-                newlist.append(flat_obs)
-        return newlist
     if indicator_data_available("BIODIV"):
         # retrieve our raw data from the database
         mongoQuery = {"collection-info.RawDataDestination": "BIODIV"}
         raw_data = parse_json(sspi_raw_api_data.find(mongoQuery))
-        # parse the observation into a clean format
+        # parse the observation(s) into a clean format
         clean_obs_dict = {}
         for country in raw_data:
             geoAreaCode = country["observation"]["geoAreaCode"]
-            if not geoAreaCode in clean_obs_dict.keys():
-                clean_obs_dict[geoAreaCode] = {}
-            series = country["observation"]["series"]
-            years_list = json.loads(country["observation"]["years"])
-            for year in years_list:
-                year_int = int(year["year"][1:5])
-                if year["value"] == '':
-                    pass
-                elif year_int not in clean_obs_dict[geoAreaCode].keys():
-                    clean_obs_dict[geoAreaCode][year_int]["Intermediates"] = {series: year["value"]}
-                else:   
-                    clean_obs_dict[geoAreaCode][year_int]["Intermediates"][series]= year["value"]
-            # after all observations have been extracted for a country
-            for year in clean_obs_dict[geoAreaCode].keys():
-                if not year in clean_obs_dict[geoAreaCode].keys() or not isinstance(year, int):
-                    pass
-                else: 
-                    if 'N' in clean_obs_dict[geoAreaCode][year]["Intermediates"].values():
-                        clean_obs_dict[geoAreaCode][year]["RAW"] = 'N'
-                    else:
-                        clean_obs_dict[geoAreaCode][year]["RAW"] = sum([float(x) for x in clean_obs_dict[geoAreaCode][year].values()])/len(clean_obs_dict[geoAreaCode][year])
+            print("geoAreaCode", int(geoAreaCode))
+            country_data = countries.get(numeric=geoAreaCode)
+            if country_data:
+                print(country_data.alpha_3)
+            else:
+                print("Could not find data for", country["observation"]["geoAreaName"])
+            # if not geoAreaCode in clean_obs_dict.keys():
+            #     clean_obs_dict[geoAreaCode] = {}
+            # series = country["observation"]["series"]
+            # years_list = json.loads(country["observation"]["years"])
+            # for year in years_list:
+            #     year_int = int(year["year"][1:5])
+            #     if year["value"] == '':
+            #         pass
+            #     elif year_int not in clean_obs_dict[geoAreaCode].keys():
+            #         clean_obs_dict[geoAreaCode][year_int]["Intermediates"] = {series: year["value"]}
+            #     else:   
+            #         clean_obs_dict[geoAreaCode][year_int]["Intermediates"][series]= year["value"]
+            # # after all observations have been extracted for a country
+            # for year in clean_obs_dict[geoAreaCode].keys():
+            #     if not year in clean_obs_dict[geoAreaCode].keys() or not isinstance(year, int):
+            #         pass
+            #     else: 
+            #         if 'N' in clean_obs_dict[geoAreaCode][year]["Intermediates"].values():
+            #             clean_obs_dict[geoAreaCode][year]["RAW"] = 'N'
+            #         else:
+            #             clean_obs_dict[geoAreaCode][year]["RAW"] = sum([float(x) for x in clean_obs_dict[geoAreaCode][year].values()])/len(clean_obs_dict[geoAreaCode][year])
         
         # process dictionary into usable format
         # clean_obs_list = flatten(clean_obs_dict, "M49CountryCode")
