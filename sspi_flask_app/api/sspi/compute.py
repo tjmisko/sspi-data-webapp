034bb9daf4c98d6d962c67b034e513a218e6d557
tjmisko <tjmisko@users.noreply.github.com>
2023-06-12 20:32:01 +0200

    sectioned off functionality of compute_biodiv into functions contained in the sspi source utility

--- sspi_flask_app/api/sspi/compute.py
+++ sspi_flask_app/api/sspi/compute.py
@@ -1,35 +1,17 @@
 import re
 from flask import Blueprint, request, render_template
 from ... import sspi_clean_api_data, sspi_raw_api_data
+from ...api.source_utilities.sdg import flatten_and_format_nested_sdg_dictionary, extract_sdg_pivot_data_to_nested_dictionary
 import json
 from bson import json_util
 from pycountry import countries
+from ..api import fetch_raw_data
 
 def parse_json(data):
     return json.loads(json_util.dumps(data))
+
 def print_json(data):
     print(json.dumps(data, indent=4, sort_keys=True))
-
-def fetch_raw_data(RawDataDestination):
-    """
-    Utility function that handles querying the database
-    """
-    mongoQuery = {"collection-info.RawDataDestination": RawDataDestination}
-    raw_data = parse_json(sspi_raw_api_data.find(mongoQuery))
-    return raw_data
-
-def format_m49_as_string(input):
-    """
-    Utility function ensuring that all M49 data is correctly formatted as a
-    string of length 3 for use with the pycountry library
-    """
-    input = int(input)
-    if input >= 100:
-        return str(input) 
-    elif input >= 10:
-        return '0' + str(input)
-    else: 
-        return '00' + str(input)
         
 
 compute_bp = Blueprint("compute_bp", __name__,
@@ -56,30 +38,7 @@ def compute_biodiv():
     if not indicator_data_available("BIODIV"):
         return "Data unavailable. Try running collect."
     raw_data = fetch_raw_data("BIODIV")
-    # pass through the raw data and extract the datapoints we want
-    intermediate_obs_dict = {}
-    for country in raw_data:
-        geoAreaCode = format_m49_as_string(country["observation"]["geoAreaCode"])
-        country_data = countries.get(numeric=geoAreaCode)
-        # make sure that the data corresponds to a valid country (gets rid of regional aggregates)
-        if not country_data:
-            continue
-        series = country["observation"]["series"]
-        annual_data_list = json.loads(country["observation"]["years"])
-        COU = country_data.alpha_3
-        # add the country to the dictionary if it's not there already
-        if COU not in intermediate_obs_dict.keys():
-            intermediate_obs_dict[COU] = {}
-        # iterate through each of the annual observations and add the appropriate entry
-        for obs in annual_data_list:
-            year = int(obs["year"][1:5])
-            if obs["value"] == '':
-                continue
-            if year not in intermediate_obs_dict[COU].keys():
-                intermediate_obs_dict[COU][year] = {}
-            intermediate_obs_dict[COU][year][series] = obs["value"]
-    
-    # process dictionary into usable format
-    print(raw_data[0])
+    intermediate_obs_dict = extract_sdg_pivot_data_to_nested_dictionary(raw_data)
+    final_data_list = flatten_and_format_nested_sdg_dictionary(intermediate_obs_dict)
     # store the cleaned data in the database
-    return json.dumps(intermediate_obs_dict)
+    return json_util.dumps(final_data_list)
