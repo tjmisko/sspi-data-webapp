df1a128a6d033dad5e0af6bff557e55942f6924f
tjmisko <tjmisko@users.noreply.github.com>
2023-06-28 18:57:06 +0200

    css overflow fix, yugh :(

--- sspi_flask_app/home/static/js/makeDynamicDataTable.js
+++ sspi_flask_app/home/static/js/makeDynamicDataTable.js
@@ -1,21 +1,34 @@
-// https://tabulator.info/docs/5.5/columns#overview
-// async function makeDynamicDataTable(IndicatorCode="BIODIV") {
-//     var country_groups = await fetch('/api/v1/metadata')
-//     var dynamic_data = await fetch('/api/v1/query/indicator/' + IndicatorCode + '?database=sspi_clean_api_data')
-//     var country_group_data = await country_groups.json()
-//     var dynamic_data_table = await dynamic_data.json()
-//     console.log(country_group_data)
-//     console.log(dynamic_data_table)
-//     var table = new Tabulator("#dynamic-data-table", {
-//         data:dynamic_data_table, //assign data to table
-//         autoColumns:true, //create columns from data field names
-//     });
-// }
+
 
 var table = new Tabulator("#dynamic-data-table", {
     ajaxURL:"/api/v1/coverage/BIODIV", //ajax URL
-    autoColumns:true, //create columns from data field names
-    height: "300px",
-    responsiveLayout:"collapse",
-    width: "100%"
+    height: "400px",
+    headerSortClickElement:"icon",
+    columns:[
+        {title: "Country Name", field:"CountryName", formatter:"string"},
+        {title: "Country Code", field:"CountryCode", formatter:"string"},
+        {title: 2000, field: "2000", formatter:"string"},
+        {title: 2001, field: "2001", formatter:"string"},
+        {title: 2002, field: "2002", formatter:"string"},
+        {title: 2003, field: "2003", formatter:"string"},
+        {title: 2004, field: "2004", formatter:"string"},
+        {title: 2005, field: "2005", formatter:"string"},
+        {title: 2006, field: "2006", formatter:"string"},
+        {title: 2007, field: "2007", formatter:"string"},
+        {title: 2008, field: "2008", formatter:"string"},
+        {title: 2009, field: "2009", formatter:"string"},
+        {title: 2010, field: "2010", formatter:"string"},
+        {title: 2011, field: "2011", formatter:"string"},
+        {title: 2012, field: "2012", formatter:"string"},
+        {title: 2013, field: "2013", formatter:"string"},
+        {title: 2014, field: "2014", formatter:"string"},
+        {title: 2015, field: "2015", formatter:"string"},
+        {title: 2016, field: "2016", formatter:"string"},
+        {title: 2017, field: "2017", formatter:"string"},
+        {title: 2018, field: "2018", formatter:"string"},
+        {title: 2019, field: "2019", formatter:"string"},
+        {title: 2020, field: "2020", formatter:"string"},
+        {title: 2021, field: "2021", formatter:"string"},
+        {title: 2022, field: "2022", formatter:"string"}
+    ]
 });
