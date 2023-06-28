55de13783a536c6d50102e5e22aa5431a595e7aa
tjmisko <tjmisko@users.noreply.github.com>
2023-06-28 15:06:16 +0200

    minor table reformatting

--- sspi_flask_app/home/static/js/makeAPICoverageReport.js
+++ sspi_flask_app/home/static/js/makeAPICoverageReport.js
@@ -2,7 +2,9 @@ var table = new Tabulator("#api-link-coverage-data-table", {
     ajaxURL:"/api/v1/api_coverage", //ajax URL
     columns:[
         {title: "Indicator Code" , field:"IndicatorCode", formatter:"textarea"},
-        {title: "Collect Method Implemented" , field:"collect_implemented", formatter: "tickCross"},
-        {title: "Compute Method Implemented" , field:"compute_implemented", formatter: "tickCross"}
+        {title: "Collect Method Implemented" , field:"collect_implemented", formatter: "tickCross", hozAlign:"center"},
+        {title: "Compute Method Implemented" , field:"compute_implemented", formatter: "tickCross", hozAlign:"center"}
     ]
 });
+
+// // https://tabulator.info/docs/5.5/columns#overview
