c70a263b0407294fa073906b40596c1a4013ec92
tjmisko <tjmisko@users.noreply.github.com>
2023-07-26 18:23:08 -0700

    linked script file to api_bp with query route

--- sspi_flask_app/api/static/script.js
+++ sspi_flask_app/api/static/script.js
@@ -1,3 +1,12 @@
 async function DatabaseStatus(database){
     let response = await fetch('/api/v1/database/' + database +'/status')
 }
+
+async function handleQuery(IndicatorCode){
+    $.get('/api/v1/IndicatorCode', 
+    (data)=>{
+        $("#" + IndicatorCode + ".results-box").children(".")
+    });
+    $("#" + IndicatorCode + ".results-box").style.display = "block"
+    $("#" + IndicatorCode + ".results-box").children(".").append("waiting")
+}
