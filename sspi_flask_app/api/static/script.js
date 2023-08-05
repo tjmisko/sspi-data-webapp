df719dc23f347401b20a3cad11f49e0833470882
tjmisko <tjmisko@users.noreply.github.com>
2023-08-04 17:27:48 -0700

    wrote compute handler and updated compute function convention to match requirements for return value of route

--- sspi_flask_app/api/static/script.js
+++ sspi_flask_app/api/static/script.js
@@ -7,7 +7,7 @@ function closeBox(obj) {
 }
 
 async function handleQuery(IndicatorCode){
-    $.get('/api/v1/query/indicator/' + IndicatorCode, 
+    $.get(`/api/v1/query/indicator/${IndicatorCode}`, 
         (data)=>{
             console.log(data)
             $("#" + IndicatorCode + ".results-box")
@@ -22,15 +22,31 @@ async function handleQuery(IndicatorCode){
 function handleCollect(IndicatorCode) {
     $(`#${IndicatorCode}.results-box`).show()
     let message_handler = new EventSource(`/api/v1/collect/${IndicatorCode}`)
-    message_handler.onmessage = function (event) {
+    console.log(`GET /api/v1/collect/${IndicatorCode}`)
+    message_handler.addEventListener("message", function (event) {
         if (event.data === "close") {
             message_handler.close()
         } else {
+            console.log(event)
             results_box.children.innerHTML = event.data
         }
-    };
+    });
     // (e) => {
     //     $(`#${IndicatorCode}.results-box`).children(".return-content").text(e.data)
     //     if (e.data === "Collection complete") {message_handler.close()}
     // }
+}
+
+function handleCompute(IndicatorCode) {
+    $.get(`/api/v1/compute/${IndicatorCode}`, 
+        (data)=>{
+            console.log(data)
+            $("#" + IndicatorCode + ".results-box")
+                .children(".return-content")
+                .empty()
+                .append(JSON.stringify(data, null, 2))
+        });
+    $(`#${IndicatorCode}.results-box`).show()
+    $("#" + IndicatorCode + ".results-box").children(".return-content").empty().append("processing")
+
 }
