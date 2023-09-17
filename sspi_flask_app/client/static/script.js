5fb4c8f76ae03351f3d4e3783db60d650d435ba1
tjmisko <tjmisko@users.noreply.github.com>
2023-09-16 20:34:36 -0700

    fighting with close button

--- sspi_flask_app/client/static/script.js
+++ sspi_flask_app/client/static/script.js
@@ -1,4 +1,5 @@
 $(".data-download-reveal").click(()=>{$(".data-download-form").slideDown();$(".data-download-reveal").slideUp();})
 $(".data-download-close").click(()=>{$(".data-download-reveal").slideDown();$(".data-download-form").slideUp();})
-async function addWidget(){await $.get('/widget',(data)=>{grid.addWidget({w:3,h:2,content:data})})}
-function removeWidget(el){grid.removeWidget($(el).parent().parent())}
+async function addWidget(){await $.get('/widget',(data)=>{grid.addWidget({w:6,h:10,minW:4,minH:5,content:data})})}
+function removeWidget(el){grid.removeWidget(el.parenElement.parentElement.parentElement)
+console.log("hello")}
