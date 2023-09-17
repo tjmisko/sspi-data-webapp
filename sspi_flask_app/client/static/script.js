f71e4a5e242a6dc548aa3ea6600d0fef6bf0e509
tjmisko <tjmisko@users.noreply.github.com>
2023-09-17 13:18:27 -0700

    testing js for fullscreen

--- sspi_flask_app/client/static/script.js
+++ sspi_flask_app/client/static/script.js
@@ -19,3 +19,6 @@ function revealWidgetOptions(){$(".widget-type-options-menu").slideToggle()}
 async function addWidget(widgettype){await $.get(`/widget/${widgettype}`,(data)=>{grid.addWidget({w:6,h:10,minW:4,minH:5,content:data,id:crypto.randomUUID()});revealWidgetOptions();})}
 function removeWidget(el){widgetId=$(el).parents().eq(2).attr('gs-id')
 grid.removeWidget($(`[gs-id=${widgetId}]`).get(0))}
+function fullscreenWidget(el){widgetId=$(el).parents().eq(2).attr('gs-id')
+console.log(grid.margin())
+console.log(widgetId)}
