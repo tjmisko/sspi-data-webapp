88a9c1c85fb4171e84b68c6031a8f7954bbc3e36
tjmisko <tjmisko@users.noreply.github.com>
2023-09-16 21:41:46 -0700

    ported over make barchart

--- sspi_flask_app/client/static/js/makeChart.js
+++ sspi_flask_app/client/static/js/makeChart.js
@@ -1,71 +1,63 @@
-// const BarChartCanvas = document.getElementById('BarChart');
-// const BarChart = new Chart(BarChartCanvas, {
-//     type: 'bar',
-//     data: {},
-//     options: {}
-// });
+raw=false
 
-// makeBarChart('BIODIV')
-// raw=false
-
-// async function makeBarChart(IndicatorCode){
-//     let response = await fetch('/api/v1/query/indicator/' + IndicatorCode)
-//     let indicator_data = await response.json()
-//     indicator_data.sort((a, b) => b.RANK - a.RANK)
-//     let y_axis = raw ? getRaw(indicator_data) : getScores(indicator_data)
-//     BarChart.data = {
-//         labels: getCountries(indicator_data),
-//         datasets: [{
-//             datacode: IndicatorCode,
-//             label: indicator_data[0].IndicatorNameShort,
-//             data: y_axis,
-//             backgroundColor: 'rgb(255, 99, 132)',
-//             borderColor: 'rgb(255, 99, 132)',
-//             borderWidth: 1
-//         }]
-//     }
+async function makeBarChart(IndicatorCode){
+    let response = await fetch('/api/v1/query/indicator/' + IndicatorCode)
+    let indicator_data = await response.json()
+    indicator_data.sort((a, b) => b.RANK - a.RANK)
+    let y_axis = raw ? getRaw(indicator_data) : getScores(indicator_data)
+    BarChart.data = {
+        labels: getCountries(indicator_data),
+        datasets: [{
+            datacode: IndicatorCode,
+            label: indicator_data[0].IndicatorNameShort,
+            data: y_axis,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            borderWidth: 1
+        }]
+    }
     
-//     BarChart.options = {
-//         scaleShowValues: true,
-//         layout: {padding : 20},
-//         scales: {
-//           xAxes: [{
-//             id: 'x',
-//             type: 'category',
-//             title: {
-//                 display: true,
-//                 text: 'Country'
-//             },
-//             ticks: {
-//               autoskip: true,
-//             }
-//           }]
-//         }
-//     }
-//     BarChart.update();
-// }
+    BarChart.options = {
+        scaleShowValues: true,
+        layout: {padding : 20},
+        scales: {
+          xAxes: [{
+            id: 'x',
+            type: 'category',
+            title: {
+                display: true,
+                text: 'Country'
+            },
+            ticks: {
+              autoskip: true,
+            }
+          }]
+        }
+    }
+    BarChart.update();
+}
 
 
-// function toggleRaw() {
-//     raw = !raw
-//     try {
-//         IndicatorCode = BarChart.data.datasets[0].datacode
-//         makeBarChart(IndicatorCode);
-//     } catch (TypeError) {
-//         console.log("No Chart Loaded!\n", TypeError)
-//         IndicatorCode = null;
-//     }
-// }
+function toggleRaw() {
+    raw = !raw
+    try {
+        IndicatorCode = BarChart.data.datasets[0].datacode
+        makeBarChart(IndicatorCode);
+    } catch (TypeError) {
+        console.log("No Chart Loaded!\n", TypeError)
+        IndicatorCode = null;
+    }
+}
 
 
-// function getCountries(indicator_data) {
-//     return indicator_data.map(data => data.Country)
-// }
+function getCountries(indicator_data) {
+    return indicator_data.map(data => data.Country)
+}
 
-// function getScores(indicator_data) {
-//     return indicator_data.map(data => data.SCORE)
-// }
+function getScores(indicator_data) {
+    return indicator_data.map(data => data.SCORE)
+}
 
-// function getRaw(indicator_data) {
-//     return indicator_data.map(data => data.RAW)
-// }
+function getRaw(indicator_data) {
+    return indicator_data.map(data => data.RAW)
+}
