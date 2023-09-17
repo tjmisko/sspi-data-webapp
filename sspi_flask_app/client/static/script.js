a51d3fe90bf4bb77d62c35a7db2b0c495eb41f16
tjmisko <tjmisko@users.noreply.github.com>
2023-09-17 14:35:36 -0700

    styling return to normal

--- sspi_flask_app/client/static/script.js
+++ sspi_flask_app/client/static/script.js
@@ -20,5 +20,13 @@ async function addWidget(widgettype){await $.get(`/widget/${widgettype}`,(data)=
 function removeWidget(el){widgetId=$(el).parents().eq(2).attr('gs-id')
 grid.removeWidget($(`[gs-id=${widgetId}]`).get(0))}
 function fullscreenWidget(el){widgetId=$(el).parents().eq(2).attr('gs-id')
-console.log(grid.margin)
-console.log(widgetId)}
+widget=$(`[gs-id=${widgetId}]`).get(0)
+repositionInfo={widgetW:$(widget).attr('gs-w'),widgetH:$(widget).attr('gs-h')}
+fullscreenHeight=Math.floor(0.95*window.innerHeight/50)
+grid.update(widget,{w:12,h:fullscreenHeight})
+window.scrollTo(0,$(widget).offset().top-10)
+fullscreenButton=$(`[gs-id=${widgetId}]`).find(".fullscreen-button").html(`<button onclick="returnWidgetToOriginalSize(this, ${repositionInfo})"class="widget-controls-button fullscreen-button"><img class="fullscreen"style="width: 32px; height: 32px"src="{{ url_for('client_bp.static', filename='/svg_assets/fullscreen.svg') }}"/></button>`)}
+function returnWidgetToOriginalSize(el,repositionInfo){$(el).parents().eq(2).attr('gs-id')
+widget=$(`[gs-id=${widgetId}]`).get(0)
+grid.update(widget,{w:repositionInfo.widgetW,h:repositionInfo.widgetH})
+fullscreenButton=$(`[gs-id=${widgetId}]`).find(".fullscreen-button").html(`<button onclick="fullscreenWidget(this)"class="widget-controls-button fullscreen-button"><img class="fullscreen"style="width: 32px; height: 32px"src="{{ url_for('client_bp.static', filename='/svg_assets/fullscreen.svg') }}"/></button>`)}
