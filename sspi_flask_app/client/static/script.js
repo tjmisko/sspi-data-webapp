95e9778ab65461516cff24b598d39bcd85c2094e
tjmisko <tjmisko@users.noreply.github.com>
2023-09-16 21:41:46 -0700

    ported over make barchart

--- sspi_flask_app/client/static/script.js
+++ sspi_flask_app/client/static/script.js
@@ -1,8 +1,5 @@
 $(".data-download-reveal").click(()=>{$(".data-download-form").slideDown();$(".data-download-reveal").slideUp();})
 $(".data-download-close").click(()=>{$(".data-download-reveal").slideDown();$(".data-download-form").slideUp();})
 async function addWidget(){await $.get('/widget',(data)=>{grid.addWidget({w:6,h:10,minW:4,minH:5,content:data,id:crypto.randomUUID()})})}
-function removeWidget(el){console.log(el)
-widgetId=$(el).parents().eq(4).attr('gs-id')
-console.log(widgetId)
-grid.removeWidget($(`[gs-id=${widgetId}]`).get(0))
-console.log(`Removed widget${widgetId}`)}
+function removeWidget(el){widgetId=$(el).parents().eq(4).attr('gs-id')
+grid.removeWidget($(`[gs-id=${widgetId}]`).get(0))}
