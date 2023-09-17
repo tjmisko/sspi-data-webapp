80b2f5b100b178a898772b3b76c6ac29ef77b5eb
tjmisko <tjmisko@users.noreply.github.com>
2023-09-16 20:34:36 -0700

    fighting with close button

--- sspi_flask_app/client/static/js/widgetHandlers.js
+++ sspi_flask_app/client/static/js/widgetHandlers.js
@@ -1,9 +1,10 @@
 async function addWidget() {
     await $.get('/widget', (data) => {
-        grid.addWidget({w:3, h:2, content: data})    
+        grid.addWidget({w:6, h:10, minW: 4, minH: 5, content: data})    
     })
 }
 
 function removeWidget(el) {
-    grid.removeWidget($(el).parent().parent())
+    grid.removeWidget(el.parenElement.parentElement.parentElement)
+    console.log("hello")
 }
