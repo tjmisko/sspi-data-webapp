53b087f445557f076b41e27116f0d4a4a292954a
tjmisko <tjmisko@users.noreply.github.com>
2023-09-17 12:32:55 -0700

    took widget add options menu out of gridstack area

--- sspi_flask_app/client/static/script.js
+++ sspi_flask_app/client/static/script.js
@@ -16,6 +16,6 @@ function getCountries(indicator_data){return indicator_data.map(data=>data.Count
 function getScores(indicator_data){return indicator_data.map(data=>data.SCORE)}
 function getRaw(indicator_data){return indicator_data.map(data=>data.RAW)}
 function revealWidgetOptions(){$(".widget-type-options-menu").slideToggle()}
-async function addWidget(widgettype){await $.get(`/widget/${widgettype}`,(data)=>{grid.addWidget({w:6,h:10,minW:4,minH:5,content:data,id:crypto.randomUUID()})})}
+async function addWidget(widgettype){await $.get(`/widget/${widgettype}`,(data)=>{grid.addWidget({w:6,h:10,minW:4,minH:5,content:data,id:crypto.randomUUID()});revealWidgetOptions();})}
 function removeWidget(el){widgetId=$(el).parents().eq(4).attr('gs-id')
 grid.removeWidget($(`[gs-id=${widgetId}]`).get(0))}
