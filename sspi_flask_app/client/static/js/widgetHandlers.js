169f3d3e4b4a5ea1616d5decafba0940a1d65b44
tjmisko <tjmisko@users.noreply.github.com>
2023-09-16 20:41:14 -0700

    added uuid

--- sspi_flask_app/client/static/js/widgetHandlers.js
+++ sspi_flask_app/client/static/js/widgetHandlers.js
@@ -1,10 +1,11 @@
 async function addWidget() {
     await $.get('/widget', (data) => {
-        grid.addWidget({w:6, h:10, minW: 4, minH: 5, content: data})    
+        grid.addWidget({w:6, h:10, minW: 4, minH: 5, content: data, id: crypto.randomUUID()})    
     })
 }
 
 function removeWidget(el) {
-    grid.removeWidget(el.parenElement.parentElement.parentElement)
-    console.log("hello")
+    widgetId = $(el).parent().parent().parent().attr('id')
+    grid.removeWidget()
+    console.log("Removed widget" + widgetID)
 }
