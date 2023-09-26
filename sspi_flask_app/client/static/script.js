b1eb001113df456f8317212f7a1f5f425fca9efb
tjmisko <tjmisko@users.noreply.github.com>
2023-09-26 16:32:14 -0700

    fixed some padding issues in widget content

--- sspi_flask_app/client/static/script.js
+++ sspi_flask_app/client/static/script.js
@@ -23,7 +23,7 @@ console.log(gsId)
 grid.addWidget({w:6,h:20,minW:4,minH:5,content:data,id:gsId});revealWidgetOptions();}).then(()=>{if(widgettype==="barchart"){setupBarChart(gsId)}});}
 function removeWidget(el){widget=$(el).parents('.grid-stack-item')
 console.log(widget.attr('gs-id'))
-grid.removeWidget(widget)}
+grid.removeWidget(widget.get(0))}
 function fullscreenWidget(el){widgetId=$(el).parents().eq(2).attr('gs-id')
 widget=$(`[gs-id=${widgetId}]`).get(0)
 widgetW=$(widget).attr('gs-w')
