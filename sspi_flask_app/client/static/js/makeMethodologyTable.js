60926b713bbd786222908f5e088adb804559c4db
tjmisko <tjmisko@users.noreply.github.com>
2023-10-01 18:27:28 -0700

    fiddling with tabulator options

--- sspi_flask_app/client/static/js/makeMethodologyTable.js
+++ sspi_flask_app/client/static/js/makeMethodologyTable.js
@@ -3,11 +3,12 @@ var dynamicDataTable = new Tabulator("#methodology-indicator-table", {
     ajaxURL:"/api/v1/query/metadata/indicator_details", //ajax URL
     headerSortClickElement:"icon",
     groupBy: ["Pillar", "Category"],
+    maxHeight: "100%",
     groupStartOpen: [true, false],
     columns: [
-        {title: "Indicator", field: "Indicator"},
-        {title: "Indicator Code", field: "IndicatorCodes"},
-        {title: "Indicator Description", field: "Description"},
+        {title: "Indicator", field: "Indicator", formatter: "textarea", width: 200},
+        {title: "Code", field: "IndicatorCodes", width: 75},
+        {title: "Indicator Description", field: "Description", formatter: "textarea", width: 400},
         // {title: "xx", field: "yy"},
         // {title: "xx", field: "yy"},
         // {title: "xx", field: "yy"},
