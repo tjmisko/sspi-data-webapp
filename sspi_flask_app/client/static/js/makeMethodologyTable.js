ea833fe2f10944cf6c831670b055de60cf7a320b
tjmisko <tjmisko@users.noreply.github.com>
2023-10-01 17:55:51 -0700

    indicator table loads

--- sspi_flask_app/client/static/js/makeMethodologyTable.js
+++ sspi_flask_app/client/static/js/makeMethodologyTable.js
@@ -1,6 +1,18 @@
 
 var dynamicDataTable = new Tabulator("#methodology-indicator-table", {
-    ajaxURL:"/api/v1/query/metadata/indicator_info", //ajax URL
+    ajaxURL:"/api/v1/query/metadata/indicator_details", //ajax URL
     headerSortClickElement:"icon",
+    groupBy: ["PillarCode", "CategoryCode"],
+    groupStartOpen: [true, false],
+    columns: [
+        {title: "Indicator", field: "Indicator"},
+        {title: "Indicator Code", field: "IndicatorCodes"},
+        {title: "Indicator Description", field: "Description"},
+        // {title: "xx", field: "yy"},
+        // {title: "xx", field: "yy"},
+        // {title: "xx", field: "yy"},
+        // {title: "xx", field: "yy"},
+        // {title: "xx", field: "yy"},
+    ] 
 });
 
