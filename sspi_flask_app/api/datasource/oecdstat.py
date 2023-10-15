2933e432f34956862fda858fcf7353e65b817062
tjmisko <tjmisko@users.noreply.github.com>
2023-10-14 20:22:34 -0700

    added code to handle error in OECD API

--- sspi_flask_app/api/datasource/oecdstat.py
+++ sspi_flask_app/api/datasource/oecdstat.py
@@ -1,12 +1,34 @@
 import requests
 import bs4 as bs
 from ..api import string_to_float, string_to_int, raw_insert_one
+import urllib3
+import ssl
 
 def collectOECDIndicator(OECDIndicatorCode, IndicatorCode, IntermediateCode="NA"):
+    class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+    # "Transport adapter" that allows us to use custom ssl_context.
+
+        def __init__(self, ssl_context=None, **kwargs):
+            self.ssl_context = ssl_context
+            super().__init__(**kwargs)
+
+        def init_poolmanager(self, connections, maxsize, block=False):
+            self.poolmanager = urllib3.poolmanager.PoolManager(
+                num_pools=connections, maxsize=maxsize,
+                block=block, ssl_context=self.ssl_context)
+    
+    def get_legacy_session():
+        ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
+        session = requests.session()
+        session.mount('https://', CustomHttpAdapter(ctx))
+        return session
+        
+
     SDMX_URL_OECD_METADATA = f"https://stats.oecd.org/RestSDMX/sdmx.ashx/GetKeyFamily/{OECDIndicatorCode}"
     SDMX_URL_OECD = f"https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/{OECDIndicatorCode}"
     yield "Sending Metadata Request to OECD SDMX API\n"
-    metadata_obj = requests.get(SDMX_URL_OECD_METADATA)
+    metadata_obj = get_legacy_session().get(SDMX_URL_OECD_METADATA)
     metadata = str(metadata_obj.content)
     yield "Metadata Received from OECD SDMX API.  Sending Data Request to OECD SDMX API\n"
     yield "Sending Data Request to OECD SDMX API\n"
