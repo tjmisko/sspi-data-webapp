5f2d2c2f130226b9c7f6e093c06f7e4b0e2ba7b5
tjmisko <tjmisko@users.noreply.github.com>
2023-10-14 19:09:50 -0700

    minor housekeeping for ilo

--- sspi_flask_app/api/datasource/ilo.py
+++ sspi_flask_app/api/datasource/ilo.py
@@ -1,9 +1,9 @@
-from ... import sspi_raw_api_data
 # import pandas as pd
 # import json
 import requests
 from datetime import datetime
 from ..api import raw_insert_one
+
 def collectILOData(ILOIndicatorCode, IndicatorCode, QueryParams="...."):
     yield "Sending Data Request to ILO API\n"
     response_obj = requests.get(f"https://www.ilo.org/sdmx/rest/data/ILO,{ILOIndicatorCode}/{QueryParams}")
