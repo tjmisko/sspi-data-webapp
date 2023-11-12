2c0e72c49481aa9650a0e601c6484658899fd152
tjmisko <tjmisko@users.noreply.github.com>
2023-11-11 18:29:02 -0800

    experiementing with authentication on intialization

--- database_connector/SSPIDatabaseConnector.py
+++ database_connector/SSPIDatabaseConnector.py
@@ -6,20 +6,6 @@ from requests.adapters import HTTPAdapter
 import urllib3
 from urllib3.util.retry import Retry
 
-
-
-class CustomHttpAdapter (requests.adapters.HTTPAdapter):
-# "Transport adapter" that allows us to use custom ssl_context.
-
-    def __init__(self, ssl_context=None, **kwargs):
-        self.ssl_context = ssl_context
-        super().__init__(**kwargs)
-
-    def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = urllib3.poolmanager.PoolManager(
-            num_pools=connections, maxsize=maxsize,
-            block=block, ssl_context=self.ssl_context)
-
 class SSPIDatabase:
     def __init__(self):
         self.token = self.get_token()
@@ -27,6 +13,7 @@ class SSPIDatabase:
         ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
         self.session = requests.session()
         self.session.mount('https://', CustomHttpAdapter(ctx))
+        self.login_session()
     
     def get_token(self):
         basedir = path.abspath(path.dirname(path.dirname(__file__)))
@@ -42,3 +29,14 @@ class SSPIDatabase:
             request_string = request_string[1:]
         return self.session.get(f"http://127.0.0.1:5000/{request_string}")
 
+class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+# "Transport adapter" that allows us to use custom ssl_context.
+
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = urllib3.poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_context=self.ssl_context)
