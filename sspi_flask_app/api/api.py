06afc646377c2f10eda49ccb65bc0494dc674e77
tjmisko <tjmisko@users.noreply.github.com>
2023-11-18 14:12:58 -0800

    debugging broken imports

--- sspi_flask_app/api/api.py
+++ sspi_flask_app/api/api.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, flash, redirect, request, url_for
-from flask_login import current_user, login_required
-from bson import json_util
+from flask import Blueprint
+from flask_login import login_required
 
 api_bp = Blueprint(
     'api_bp', __name__,
@@ -8,3 +7,9 @@ api_bp = Blueprint(
     static_folder='static',
     url_prefix='/api/v1'
 )
+
+
+@api_bp.route("/")
+@login_required
+def index():
+    return "Hello World!"
