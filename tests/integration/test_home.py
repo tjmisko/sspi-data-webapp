65275def1942c99eae07dc212fdb42057fb4ea28
tjmisko <tjmisko@users.noreply.github.com>
2023-11-23 15:25:25 -0800

    setup some basic tests

--- tests/integration/test_home.py
+++ tests/integration/test_home.py
@@ -7,6 +7,5 @@ def test_home_load(client):
     assert b'<header class="site-header">' in response.data
     assert b'<body>' in response.data
     assert b'<footer class="site-footer">' in response.data
-    response = client.get(url_for('home'))
+    response = client.get(url_for('client_bp.home'))
     assert response.status_code == 200
-    assert b'home.html' in response.data
