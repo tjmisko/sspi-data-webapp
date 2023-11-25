b46ab3673c57f2f943476bbeb5a9565f42b92a74
tjmisko <tjmisko@users.noreply.github.com>
2023-11-24 23:27:41 -0800

    created save all route

--- sspi_flask_app/api/core/save.py
+++ sspi_flask_app/api/core/save.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 import json
 import os
-from flask import current_app as app
+from flask import Response, current_app as app
 from flask import Blueprint, request
 from flask_login import login_required
 
 from sspi_flask_app.models.database import SSPIRawAPIData
 
 from ..resources.utilities import lookup_database, parse_json
-from ... import sspi_bulk_data
+from ... import sspidb
 from ..resources.validators import validate_observation_list
 
 save_bp = Blueprint("save_bp", __name__,
@@ -28,8 +28,17 @@ def save_database(database_name):
     snapshots_path = os.path.join(os.path.dirname(app.instance_path), f"snapshots/{datetime_str}")
     if not os.path.exists(snapshots_path):
         os.mkdir(snapshots_path)
-    print(snapshots_path)
-    # os.mkdir(f"snapshots/{datetime_str}")
-    # with open(f"snapshots/ {datetime_str}.json", "w") as f:
-        # json.dump(database_contents, f)
+    with open(f"{snapshots_path}/{datetime_str} - {database_name}.json", "w+") as f:
+        json.dump(database_contents, f)
     return database_contents
+
+@save_bp.route("/save", methods=["GET"])
+def save_all():
+    """
+    Creates a local snapshot of all SSPI databases
+    """
+    def save_iterator():
+        for i, database_name in enumerate(sspidb.list_collection_names()):
+            yield f"Saving {database_name} to local ({i}/{len(sspidb.list_collection_names())})"
+            save_database(database_name)
+    return Response(save_iterator(), mimetype="text/event-stream")
