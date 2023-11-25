e79cd4494324c02e9f845f98a6d4a8d07b334ac1
tjmisko <tjmisko@users.noreply.github.com>
2023-11-24 19:48:51 -0800

    setting up the databases

--- sspi_flask_app/__init__.py
+++ sspi_flask_app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import MongoClient
 from flask_bcrypt import Bcrypt
 from flask_assets import Environment
+from sspi_flask_app.models.database import MongoWrapper
 from .assets import compile_static_assets
 
 db = SQLAlchemy()
@@ -20,15 +21,16 @@ limiter = Limiter(
 
 client = MongoClient('localhost', 27017)
 sspidb = client.flask_db
-sspi_main_data_v3 = sspidb.sspi_main_data_v3
-sspi_raw_api_data = sspidb.sspi_raw_api_data
-sspi_bulk_data = sspidb.sspi_bulk_data
-sspi_clean_api_data = sspidb.sspi_clean_api_data
-sspi_imputed_data = sspidb.sspi_imputed_data
-sspi_bulk_data = sspidb.sspi_bulk_data
-sspi_analysis = sspidb.sspi_analysis
-sspi_metadata = sspidb.sspi_metadata
-sspi_dynamic_data = sspidb.sspi_dynamic_data
+
+sspi_main_data_v3 = MongoWrapper(sspidb.sspi_main_data_v3)
+sspi_raw_api_data = MongoWrapper(sspidb.sspi_raw_api_data)
+sspi_bulk_data = MongoWrapper(sspidb.sspi_bulk_data)
+sspi_clean_api_data = MongoWrapper(sspidb.sspi_clean_api_data)
+sspi_imputed_data = MongoWrapper(sspidb.sspi_imputed_data)
+sspi_bulk_data = MongoWrapper(sspidb.sspi_bulk_data)
+sspi_analysis = MongoWrapper(sspidb.sspi_analysis)
+sspi_metadata = MongoWrapper(sspidb.sspi_metadata)
+sspi_dynamic_data = MongoWrapper(sspidb.sspi_dynamic_data)
 
 assets = Environment()
 
--- sspi_flask_app/models/database.py
+++ sspi_flask_app/models/database.py
@@ -2,6 +2,7 @@ from .errors import InvalidObservationFormatError
 class MongoWrapper:
     def __init__(self, mongo_database):
         self._mongo_database = mongo_database
+        self.name = mongo_database.name
     
     def validate_document_format(self, document: dict, observation_number:int=None):
         if not "IndicatorCode":
@@ -43,16 +44,28 @@ class MongoWrapper:
 class SSPIRawAPIData(MongoWrapper):
     def __init__(self, mongo_database):
         super().__init__(mongo_database)
-        self.name = "sspi_raw_api_data"
     
     def validate_document_format(self, document: dict, observation_number:int=None):
+        """
+        Raises an error if the document is not in the valid
+
+        Valid Document Format:
+            {
+                "IndicatorCode": str,
+                "Raw": str or dict or int or float,
+                "CollectedAt": datetime,
+                ...
+            }
+        The fields IndicatorCode, Raw, and CollectedAt are required.
+        Additional fields are allowed, but not required.
+        """
         if not "IndicatorCode":
             raise InvalidObservationFormatError(f"'IndicatorCode' is a required argument (observation {observation_number})")
         if not "Raw":
             raise InvalidObservationFormatError(f"'Raw' is a required argument (observation {observation_number})")
         if not "CollectedAt":
             raise InvalidObservationFormatError(f"'CollectedAt' is a required argument (observation {observation_number})")
-        return type(document) is dict
+        
 
 
     
