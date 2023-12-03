252a40aa036827083852114952de8021a5c8a20a
Minh Nguyen <minh.pnguyen@berkeley.edu>
2023-12-03 13:13:10 -0800

    lljkadded kwargs to raw_insert_one

--- sspi_flask_app/api/datasource/iea.py
+++ sspi_flask_app/api/datasource/iea.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import requests
 from ..resources.adapters import raw_insert_many
 
-def collectIEAData(IEAIndicatorCode, IndicatorCode, IntermediateCode="NA"):
+def collectIEAData(IEAIndicatorCode, IndicatorCode, IntermediateCode="NA", Username="NA"):
     response = requests.get(f"https://api.iea.org/stats/indicator/{IEAIndicatorCode}").json()
-    count = raw_insert_many(response, IndicatorCode, IntermediateCode)
+    count = raw_insert_many(response, IndicatorCode, IntermediateCode, Username)
     yield f"Successfully inserted {count} observations into the database"
--- sspi_flask_app/api/resources/adapters.py
+++ sspi_flask_app/api/resources/adapters.py
@@ -6,31 +6,29 @@ from datetime import datetime
 # Collect Storage Utilities #
 #############################
 
-def raw_insert_one(observation, IndicatorCode, IntermediateCode="NA", Metadata="NA"):
+def raw_insert_one(observation, IndicatorCode, **kwargs):
     """
     Utility Function the response from an API call in the database
     - Observation to be passed as a well-formed dictionary for entry into pymongo
     - IndicatorCode is the indicator code for the indicator that the observation is for
     """
-    sspi_raw_api_data.insert_one({
-        "collection-info": {
-            "IndicatorCode": IndicatorCode,
-            "IntermediateCodeCode": IntermediateCode,
-            "Metadata": Metadata,
-            "CollectedAt": datetime.now()
-        },
-        "observation": observation
-    })
+    document = {
+        "IndicatorCode": IndicatorCode,
+        "Raw": observation, 
+        "CollectAt": datetime.now()
+    }
+    document.update(kwargs)
+    sspi_raw_api_data.insert_one()
     return 1
     
-def raw_insert_many(observation_list, IndicatorCode, IntermediateCode="NA", Metadata="NA"):
+def raw_insert_many(observation_list, IndicatorCode, **kwargs):
     """
     Utility Function 
     - Observation to be past as a list of well form observation dictionaries
     - IndicatorCode is the indicator code for the indicator that the observation is for
     """
     for i, observation in enumerate(observation_list):
-        raw_insert_one(observation, IndicatorCode, IntermediateCode, Metadata)
+        raw_insert_one(observation, IndicatorCode, **kwargs)
     return i+1
 
 def fetch_raw_data(IndicatorCode):
