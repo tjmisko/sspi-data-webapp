14d917c3c60efda7a31b16c55b954d8f83579c94
tjmisko <tjmisko@users.noreply.github.com>
2023-12-03 16:59:40 -0800

    removed some duplicated validators from the load.py route

--- sspi_flask_app/api/core/load.py
+++ sspi_flask_app/api/core/load.py
@@ -2,9 +2,7 @@ import json
 from flask import Blueprint, request
 from flask_login import login_required
 
-from ..resources.utilities import parse_json
 from ... import sspi_bulk_data
-from ..resources.validators import validate_observation_list
 
 load_bp = Blueprint("load_bp", __name__,
                     template_folder="templates", 
@@ -19,8 +17,6 @@ def load(IndicatorCode):
     """
     observations_list = json.loads(request.get_json())
     print(type(observations_list))
-    ### Check that observations match the expected format and declared IndicatorCode
-    validate_observation_list(observations_list, "sspi_bulk_data", IndicatorCode)
     ### If format valid, insert
     count = sspi_bulk_data.insert_many(observations_list)
     return f"Inserted {count} observations into database."
