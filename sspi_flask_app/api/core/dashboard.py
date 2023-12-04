d67b239dfc16956bc32bb039d0924870a7e9dde9
tjmisko <tjmisko@users.noreply.github.com>
2023-12-04 00:43:52 -0800

    revising more dashboard issues

--- sspi_flask_app/api/core/dashboard.py
+++ sspi_flask_app/api/core/dashboard.py
@@ -55,7 +55,7 @@ def api_coverage():
     """
     Return a list of all endpoints and whether they are implemented
     """
-    all_indicators = indicator_codes()
+    all_indicators = sspi_metadata.indicator_codes()
     endpoints = [str(r) for r in app.url_map.iter_rules()]
     collect_implemented = [re.search(r'(?<=api/v1/collect/)(?!static)([\w]*)', r).group() for r in endpoints if re.search(r'(?<=api/v1/collect/)(?!static)[\w]*', r)]
     compute_implemented = [re.search(r'(?<=api/v1/compute/)(?!static)[\w]*', r).group() for r in endpoints if re.search(r'(?<=api/v1/compute/)(?!static)[\w]*', r)]
@@ -70,16 +70,17 @@ def get_dynamic_data(IndicatorCode):
     """
     Use the format argument to control whether the document is formatted for the website table
     """
-    request_country_group = request.args.get("country_group", default = "sspi_67", type = str)
-    country_codes = country_group(request_country_group)
-    query_results = parse_json(sspi_clean_api_data.find({"IndicatorCode": IndicatorCode, "CountryCode": {"$in": country_codes}},
-                                                        {"_id": 0, "Intermediates": 0, "IndicatorCode": 0}))
+    CountryGroup = request.args.get("CountryGroup", default = "SSPI49", type = str)
+    query_results = sspi_clean_api_data.find(
+        {"IndicatorCode": IndicatorCode, "CountryGroup": CountryGroup},
+        options={"_id": 0, "Intermediates": 0, "IndicatorCode": 0}
+    )
     print(query_results)
     long_data = pd.DataFrame(query_results).drop_duplicates()
-    long_data = long_data.astype({"YEAR": int, "RAW": float})
+    long_data = long_data.astype({"Year": int, "Value": float})
     long_data = long_data.round(3)
-    wide_dataframe = pd.pivot(long_data, index="CountryCode", columns="YEAR", values="RAW")
-    nested_data = json.loads(wide_dataframe.to_json(orient="index"))
+    wide_dataframe = pd.pivot(long_data, index="CountryCode", columns="Year", values="Value")
+    nested_data = json.loads(str(wide_dataframe.to_json(orient="index")))
     return_data = []
     for country_code in nested_data.keys():
         country_data = nested_data[country_code]
