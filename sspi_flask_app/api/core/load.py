4c80e882bd9e663c4cdd22ad85396ae578c8e358
tjmisko <tjmisko@users.noreply.github.com>
2023-12-04 00:54:08 -0800

    minor edits to loady.py

--- sspi_flask_app/api/core/load.py
+++ sspi_flask_app/api/core/load.py
@@ -8,12 +8,6 @@ load_bp = Blueprint("load_bp", __name__,
                     template_folder="templates", 
                     static_folder="static")
 
-@load_bp.route("/load/sspi_main_data_v3", methods=['GET'])
-@login_required
-def load_maindata():
-    count = sspi_main_data_v3.load()
-    return f"Inserted {count} metadata documents into database."
-
 @load_bp.route("/load/<IndicatorCode>", methods=["POST"])
 @login_required
 def load(IndicatorCode):
@@ -23,6 +17,12 @@ def load(IndicatorCode):
     observations_list = json.loads(request.get_json())
     count = sspi_bulk_data.insert_many(observations_list)
     return f"Inserted {count} observations into database for Indicator {IndicatorCode}."
+
+@load_bp.route("/load/sspi_main_data_v3", methods=['GET'])
+@login_required
+def load_maindata():
+    count = sspi_main_data_v3.load()
+    return f"Inserted {count} main data documents into database."
 
 @load_bp.route("/load/sspi_metadata", methods=['GET'])
 @login_required
