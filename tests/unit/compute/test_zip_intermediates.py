962d47ee75512cbfd9cd9c1a86b368c493a5debb
tjmisko <tjmisko@users.noreply.github.com>
2024-02-25 13:13:32 -0800

    Drafted integration test for zip_intermediates

--- tests/unit/compute/test_zip_intermediates.py
+++ tests/unit/compute/test_zip_intermediates.py
@@ -6,12 +6,47 @@ from sspi_flask_app.models.errors import InvalidDocumentFormatError
 @pytest.fixture
 def test_data():
     yield [
-        {"IntermediateCode": "TERRST", "CountryCode": "AUS", "Year": 2018, "Value": 50, "Unit": "Index"},
-        {"IntermediateCode": "FRSHWT", "CountryCode": "AUS", "Year": 2018, "Value": 50, "Unit": "Index"},
-        {"IntermediateCode": "MARINE", "CountryCode": "AUS", "Year": 2018, "Value": 50, "Unit": "Index"},
-        {"IntermediateCode": "TERRST", "CountryCode": "URU", "Year": 2018, "Value": 50, "Unit": "Index"},
-        {"IntermediateCode": "FRSHWT", "CountryCode": "URU", "Year": 2018, "Value": 50, "Unit": "Index"},
-        {"IntermediateCode": "MARINE", "CountryCode": "URU", "Year": 2018, "Value": 50}
+      {
+        "IntermediateCode": "TERRST",
+        "CountryCode": "AUS",
+        "Year": 2018,
+        "Value": 50,
+        "Unit": "Index"
+      },
+      {
+        "IntermediateCode": "FRSHWT",
+        "CountryCode": "AUS",
+        "Year": 2018,
+        "Value": 50,
+        "Unit": "Index"
+      },
+      {
+        "IntermediateCode": "MARINE",
+        "CountryCode": "AUS",
+        "Year": 2018,
+        "Value": 50,
+        "Unit": "Index"
+      },
+      {
+        "IntermediateCode": "TERRST",
+        "CountryCode": "URU",
+        "Year": 2018,
+        "Value": 50,
+        "Unit": "Index"
+      },
+      {
+        "IntermediateCode": "FRSHWT",
+        "CountryCode": "URU",
+        "Year": 2018,
+        "Value": 50,
+        "Unit": "Index"
+      },
+      {
+        "IntermediateCode": "MARINE",
+        "CountryCode": "URU",
+        "Year": 2018,
+        "Value": 50
+      }
     ]
 
 def test_validate_intermediates_list(test_data):
@@ -51,3 +86,16 @@ def test_group_by_indicator(test_data):
     assert indicator_document_list[1]["Year"] == 2018
     assert indicator_document_list[1]["Intermediates"][0]["IntermediateCode"] == "TERRST"
     assert indicator_document_list[1]["Intermediates"][1]["IntermediateCode"] == "FRSHWT"
+
+def test_zip_intermediates(test_data):
+    zipped_list = zip_intermediates(test_data,
+                                    "BIODIV",
+                                   ScoreFunction=lambda TERRST, FRSHWT, MARINE: TERRST/3 + FRSHWT/3 + MARINE/3,
+                                   ScoreBy="Score")
+    assert len(zipped_list) == 2
+    assert zipped_list[0]["CountryCode"] == "AUS"
+    assert zipped_list[0]["Year"] == 2018
+    assert zipped_list[0]["IndicatorCode"] == "BIODIV"
+    assert zipped_list[1]["CountryCode"] == "URU"
+    assert zipped_list[1]["Year"] == 2018
+    assert zipped_list[1]["IndicatorCode"] == "BIODIV"
