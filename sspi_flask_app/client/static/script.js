c88eafe18ab76a1c6f021655bf581e016a5c935e
Tristan Misko <tristanmisko@gmail.com>
2024-09-18 13:02:56 -0700

    Actions: Removed Secret

--- sspi_flask_app/client/static/script.js
+++ sspi_flask_app/client/static/script.js
@@ -1,3 +1,5 @@
+async function fetchComparisonData(country1,country2,country3){country_data=await fetch(`/api/v1/query/sspi_main_data_v3?CountryCode=${country1}&CountryCode=${country2}&CountryCode=${country3}`)}
+function categoryComparison(chartCanvas,categoryCode,country_data){const chartConfig={type:'bar',data:data,options:{plugins:{title:{display:true,text:'Comparison of Category Scores'},},responsive:true,scales:{x:{stacked:true,},y:{stacked:true}}}};}
 $(".data-download-reveal").click(()=>{$(".data-download-form").slideDown();$(".data-download-reveal").slideUp();})
 $(".data-download-close").click(()=>{$(".data-download-reveal").slideDown();$(".data-download-form").slideUp();})
 async function getStaticData(IndicatorCode){const response=await fetch(`/api/v1/static/${IndicatorCode}`)
