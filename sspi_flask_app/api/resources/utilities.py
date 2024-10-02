cb85342296842b1b55e31b7c91aeff9c253e77c9
Tristan Misko <tristanmisko@gmail.com>
2024-10-02 13:25:22 -0700

    fix: Adjusted the Database Lookup to Account for Backend Changes

--- sspi_flask_app/api/resources/utilities.py
+++ sspi_flask_app/api/resources/utilities.py
@@ -5,7 +5,7 @@ from flask import jsonify
 import pandas as pd
 import inspect
 import math
-from ... import sspi_main_data_v3, sspi_bulk_data, sspi_raw_api_data, sspi_clean_api_data, sspi_imputed_data, sspi_metadata
+from ... import sspi_main_data_v3, sspi_bulk_data, sspi_raw_api_data, sspi_clean_api_data, sspi_imputed_data, sspi_metadata, sspi_static_radar_data, sspi_dynamic_line_data
 from sspi_flask_app.models.errors import InvalidDatabaseError
 
 def format_m49_as_string(input):
@@ -52,6 +52,10 @@ def lookup_database(database_name):
         return sspi_imputed_data
     elif database_name == "sspi_metadata":
         return sspi_metadata
+    elif database_name == "sspi_static_radar_data":
+        return sspi_static_radar_data
+    elif database_name == "sspi_dynamic_line_data":
+        return sspi_dynamic_line_data
     raise InvalidDatabaseError(database_name)
 
 def string_to_float(string):
