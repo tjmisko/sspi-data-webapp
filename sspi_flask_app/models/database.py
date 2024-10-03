27c6c2110dd9612e9c3da1575416a7f5e07c6ef4
Tristan Misko <tristanmisko@gmail.com>
2024-10-03 11:20:50 -0700

    feat: Implemented sspi_characteristics_database

--- sspi_flask_app/models/database.py
+++ sspi_flask_app/models/database.py
@@ -87,8 +87,8 @@ class MongoWrapper:
                 "Value": 42.3005 (float or int)
                 "Unit": "MILLION_HA", (type: str) 
                 "Intermediates": {
-                    "TERRST": 9.7, (float or int)
-                    "FRSHWT": 9.7, (float or int)
+                    "TERRST": dict (see below for format)
+                    "FRSHWT": dict (see below for format)
                     ...
                 }
             ...
@@ -597,3 +597,26 @@ class SSPIStaticRadarData(MongoWrapper):
         """
         # self.validate_endpoint(document, document_number)
         pass
+
+
+class SSPICountryCharacteristics(MongoWrapper):
+    def validate_document_format(self, document: dict, document_number: int=0):
+        """
+        Raises an InvalidDocumentFormatError if the document is not in the
+        valid format
+
+        Valid Document Format:
+            {
+                "CountryCode": str,
+                "IntemediateCode": str,
+                "Year": int,
+                "Value": float,
+                "Unit": str
+            }
+        Additional fields are allowed but not required
+        """
+        self.validate_country_code(document, document_number)
+        self.validate_intermediate_code(document, document_number)
+        self.validate_year(document, document_number)
+        self.validate_value(document, document_number)
+        self.validate_unit(document, document_number)
