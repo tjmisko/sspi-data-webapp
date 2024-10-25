f7374a9bb9a106aa2fc210212e4131d63799cb3e
Tristan Misko <tristanmisko@gmail.com>
2024-10-25 15:05:57 -0700

    refactor: Extracted Old Static Bar Chart into File

--- sspi_flask_app/client/static/charts/category-differential-static.js
+++ sspi_flask_app/client/static/charts/category-differential-static.js
@@ -1,11 +1,39 @@
-async function getPillarDifferentialStatic(BaseCountry, ComparisonCountry, PillarCode) {
-    const response = await fetch(`/api/v1/static/differential/pillar/${PillarCode}?BaseCountry=${BaseCountry}&ComparisonCountry=${ComparisonCountry}`);
-    return response.json();
-}
 
+
+class StaticPillarDifferentialChart {
+    constructor(BaseCountry, ComparisonCountry, PillarCode, root) {
+        this.BaseCountry = BaseCountry;
+        this.ComparisonCountry = ComparisonCountry;
+        this.PillarCode = PillarCode;
+        this.canvas = canvas;
+        this.chart = null;
+    }
+
+    async fetch(BaseCountry, ComparisonCountry, PillarCode) {
+        const response = await fetch(`/api/v1/static/differential/pillar/${PillarCode}?BaseCountry=${BaseCountry}&ComparisonCountry=${ComparisonCountry}`);
+        return response.json();
+    }
+    initChartJSCanvas() {
+        this.canvas = document.createElement('canvas')
+        this.canvas.id = `pillar-differential-canvas-${this.PillarCode}-${this.BaseCountry}-${this.ComparisonCountry}`
+        this.canvas.width = 200
+        this.canvas.height = 200
+        this.context = this.canvas.getContext('2d')
+        this.root.appendChile(this.canvas)
+    }
+
+    update(data) {
+        this.chart.data = data
+        this.chart.labels = data.labels
+        this.chart.update()
+    }
+
+
+}
 async function pillarDifferentialStatic(BaseCountry, ComparisonCountry, PillarCode, canvas) {
-    let diff_data = await getPillarDifferentialStatic(BaseCountry, ComparisonCountry, PillarCode).then(data => data);
-    console.log(diff_data);
+    diff_data.by_category.forEach(cat => {
+        cat.x = cat.Diff;
+    })
 
     // const data = 
     const ctx = canvas.getContext('2d');
@@ -13,7 +41,15 @@ async function pillarDifferentialStatic(BaseCountry, ComparisonCountry, PillarCo
         type: 'bar',
         data: {
             labels: diff_data.by_category.map(item => item.CategoryCode),
-            datasets: diff_data.by_category
+            datasets: diff_data.by_category.forEach(cat => {
+                return {
+                    label: cat.CategoryCode,
+                    data: cat.Diff,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }
+            })
         },
         options: {
             indexAxis: 'y',
