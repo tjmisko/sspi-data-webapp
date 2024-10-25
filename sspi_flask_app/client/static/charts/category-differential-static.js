25f3d92068cc9e9030da2eac178051766e17add2
Tristan Misko <tristanmisko@gmail.com>
2024-10-25 16:58:09 -0700

    feat: Chart Styling and Structuring

--- sspi_flask_app/client/static/charts/category-differential-static.js
+++ sspi_flask_app/client/static/charts/category-differential-static.js
@@ -1,86 +1,108 @@
-
-
 class StaticPillarDifferentialChart {
-    constructor(BaseCountry, ComparisonCountry, PillarCode, root) {
+    constructor(BaseCountry, ComparisonCountry, PillarCode, parentElement) {
+        this.parentElement = parentElement;
         this.BaseCountry = BaseCountry;
         this.ComparisonCountry = ComparisonCountry;
         this.PillarCode = PillarCode;
-        this.canvas = canvas;
-        this.chart = null;
+        this.titleString = `Sustainability Score Differences (${ComparisonCountry} - ${BaseCountry}`
+
+        this.initRoot()
+        this.initChartJSCanvas()
+        this.fetch().then(data => {
+            this.update(data)
+        })
     }
 
-    async fetch(BaseCountry, ComparisonCountry, PillarCode) {
-        const response = await fetch(`/api/v1/static/differential/pillar/${PillarCode}?BaseCountry=${BaseCountry}&ComparisonCountry=${ComparisonCountry}`);
-        return response.json();
-    }
-    initChartJSCanvas() {
-        this.canvas = document.createElement('canvas')
-        this.canvas.id = `pillar-differential-canvas-${this.PillarCode}-${this.BaseCountry}-${this.ComparisonCountry}`
-        this.canvas.width = 200
-        this.canvas.height = 200
-        this.context = this.canvas.getContext('2d')
-        this.root.appendChile(this.canvas)
-    }
-
-    update(data) {
-        this.chart.data = data
-        this.chart.labels = data.labels
-        this.chart.update()
-    }
-
-
-}
-async function pillarDifferentialStatic(BaseCountry, ComparisonCountry, PillarCode, canvas) {
-    diff_data.by_category.forEach(cat => {
-        cat.x = cat.Diff;
-    })
-
-    // const data = 
-    const ctx = canvas.getContext('2d');
-    const config = {
-        type: 'bar',
-        data: {
-            labels: diff_data.by_category.map(item => item.CategoryCode),
-            datasets: diff_data.by_category.forEach(cat => {
-                return {
-                    label: cat.CategoryCode,
-                    data: cat.Diff,
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                }
-            })
-        },
-        options: {
-            indexAxis: 'y',
-            responsive: true,
-            parsing:
-            {
-                xAxisKey: 'Diff',
-                yAxisKey: 'CategoryCode'
-            },
-            scales: {
-                x: {
-                    title: {
-                        display: true,
-                        text: 'Score Difference (Base - Comparison)'
-                    },
-                    min: -1,
-                    max: 1,
-                },
-                y: {
-                    title: {
-                        display: true,
-                        text: 'Categories'
-                    },
-                    type: 'category',
-                    reverse: false
-                }
-            }
+    colormap(diff) {
+        if (diff > 0) {
+            return "#32CD3299"
+        } else {
+            return "#FF634799"
         }
     }
 
-    var StaticPillarDifferentialChart = new Chart(ctx, config);
-    StaticPillarDifferentialChart.update();
-    return StaticPillarDifferentialChart
+    async fetch() {
+        const response = await fetch(`/api/v1/static/differential/pillar/${this.PillarCode}?BaseCountry=${this.BaseCountry}&ComparisonCountry=${this.ComparisonCountry}`);
+        return response.json();
+    }
+
+    initRoot() {
+        // Create the root element
+        this.root = document.createElement('div')
+        this.root.classList.add('chart-section-pillar-differential')
+        this.parentElement.appendChild(this.root)
+    }
+
+    initChartJSCanvas() {
+        this.canvas = document.createElement('canvas')
+        this.canvas.id = `pillar-differential-canvas-${this.PillarCode}-${this.BaseCountry}-${this.ComparisonCountry}`
+        this.canvas.width = 450
+        this.canvas.height = 300
+        this.context = this.canvas.getContext('2d')
+        this.root.appendChild(this.canvas)
+        this.chart = new Chart(this.context, {
+            type: 'bar',
+            options: {
+                indexAxis: 'y',
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: false,
+                    },
+                },
+                parsing:
+                {
+                    xAxisKey: 'Diff',
+                    yAxisKey: 'CategoryCode'
+                },
+                scales: {
+                    x: {
+                        beginAtZero: true,
+                        grid: {
+                            drawTicks: false
+                        },
+                        ticks: {
+                            color: '#bbb',
+                            stepSize: 0.1
+                        },
+                        title: {
+                            display: true,
+                            color: '#bbb',
+                            text: 'Score Difference (Base - Comparison)'
+                        },
+                        min: -1,
+                        max: 1,
+                    },
+                    y: {
+                        ticks: {
+                            color: '#bbb',
+                            minRotation: 90,
+                            maxRotation: 90,
+                            align: 'center',
+                            crossAlign: 'center',
+                        },
+                        title: {
+                            padding: 10,
+                            display: true,
+                            text: 'Categories',
+                            color: '#bbb',
+                        },
+                        type: 'category',
+                        reverse: false
+                    }
+                }
+            }
+        })
+    }
+
+    update(data) {
+        data.datasets.forEach(dataset => {
+            dataset.backgroundColor = dataset.data.map(item => this.colormap(item.Diff)) // Assign colors dynamically
+            dataset.borderColor = dataset.data.map(item => this.colormap(item.Diff).slice(0, -2)) // Assign colors dynamically
+            dataset.borderWidth = 1
+        })
+        this.chart.data.datasets = data.datasets
+        this.chart.labels = data.labels
+        this.chart.update()
+    }
 }
