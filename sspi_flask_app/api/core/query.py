071c8e1909bc3f621000e9bd4acbcb0b8574d1a5
Tristan Misko <tristanmisko@gmail.com>
2024-11-23 17:28:56 -0800

    style: Adjustments to Whitespace

--- sspi_flask_app/api/core/query.py
+++ sspi_flask_app/api/core/query.py
@@ -6,9 +6,10 @@ from ..resources.utilities import parse_json, lookup_database
 from sspi_flask_app.models.database import sspi_metadata
 
 query_bp = Blueprint("query_bp", __name__,
-                     template_folder="templates", 
-                     static_folder="static", 
+                     template_folder="templates",
+                     static_folder="static",
                      url_prefix="/query")
+
 
 @query_bp.route("/<database_string>")
 def query_database(database_string):
@@ -17,21 +18,22 @@ def query_database(database_string):
     database = lookup_database(database_string)
     return jsonify(parse_json(database.find(query_params, options={"_id": 0})))
 
+
 def get_query_params(request, requires_database=False):
     """
-    Implements the logic of query parameters and raises an 
+    Implements the logic of query parameters and raises an
     InvalidQueryError for invalid queries.
 
     In Flask, request.args is a MultiDict object of query parameters, but
     I wanted the function to work for simple dictionaries as well so we can
     use it easily internally
-    
+
     Sanitizes User Input and returns a MongoDB query dictionary.
 
     Should always be implemented inside of a try except block
     with an except that returns a 404 error with the error message.
 
-    requires_database determines whether the query 
+    requires_database determines whether the query
     """
     raw_query_input = {
         "IndicatorCode": request.args.getlist("IndicatorCode"),
@@ -47,34 +49,42 @@ def get_query_params(request, requires_database=False):
     validated_query_input = validate_data_query(raw_query_input)
     return build_mongo_query(validated_query_input)
 
+
 def build_mongo_query(raw_query_input):
     """
     Given a safe and logically valid query input, build a mongo query
     """
     mongo_query = {}
-    if raw_query_input["IndicatorCode"]: 
-        mongo_query["IndicatorCode"] = {"$in": raw_query_input["IndicatorCode"]}
+    if raw_query_input["IndicatorCode"]:
+        mongo_query["IndicatorCode"] = {
+            "$in": raw_query_input["IndicatorCode"]}
     if raw_query_input["IndicatorGroup"]:
-        mongo_query["IndicatorGroup"] = {"$in": indicator_group(raw_query_input["IndicatorGroup"])}
+        mongo_query["IndicatorGroup"] = {
+            "$in": indicator_group(raw_query_input["IndicatorGroup"])}
     if raw_query_input["CountryCode"]:
         mongo_query["CountryCode"] = {"$in": raw_query_input["CountryCode"]}
     if raw_query_input["CountryGroup"]:
-        mongo_query["CountryCode"] = {"$in": country_group(raw_query_input["CountryGroup"])}
+        mongo_query["CountryCode"] = {
+            "$in": country_group(raw_query_input["CountryGroup"])}
     if raw_query_input["Year"]:
         mongo_query["YEAR"] = {"$in": raw_query_input["Year"]}
     return mongo_query
+
 
 @query_bp.route("/metadata/country_groups", methods=["GET"])
 def query_country_groups():
     return sspi_metadata.country_groups()
 
+
 @query_bp.route("/metadata/indicator_codes", methods=["GET"])
 def query_indicator_codes():
     return jsonify(sspi_metadata.indicator_codes())
 
+
 @query_bp.route("/metadata/indicator_details")
 def query_indicator_details():
     return jsonify(sspi_metadata.indicator_details())
+
 
 @query_bp.route("/metadata/intermediate_details")
 def query_intermediate_details():
