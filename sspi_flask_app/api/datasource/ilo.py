49dadb1fdef8296fc247d0d7359d6c94c4b7cc40
Tristan Misko <tristanmisko@gmail.com>
2024-11-28 15:31:59 -0800

    fix: Removed commented function version

--- sspi_flask_app/api/datasource/ilo.py
+++ sspi_flask_app/api/datasource/ilo.py
@@ -1,17 +1,7 @@
 import requests
 from sspi_flask_app.models.database import sspi_raw_api_data
 import json
-from flask import jsonify
 import re
-
-# def collectILOData(ILOIndicatorCode, IndicatorCode, QueryParams="....", **kwargs):
-#     yield "Sending Data Request to ILO API\n"
-#     response_obj = requests.get(f"https://www.ilo.org/sdmx/rest/data/ILO,{ILOIndicatorCode}/{QueryParams}")
-#     print(str(response_obj.content))
-#     observation = str(response_obj.content)
-#     yield "Data Received from ILO API.  Storing Data in SSPI Raw Data\n"
-#     count = sspi_raw_api_data.raw_insert_one(observation, IndicatorCode, **kwargs)
-#     yield f"Inserted {count} observations into the database."
 
 
 def collectILOData(ILOIndicatorCode, IndicatorCode, QueryParams="....", **kwargs):
