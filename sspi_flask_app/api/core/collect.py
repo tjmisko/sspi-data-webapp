5c3e6321cbf4ba0bd96048f83d731c9f4933ae57
Tristan Misko <tristanmisko@gmail.com>
2024-11-28 16:34:40 -0800

    feat: Implemented Generalized ILO Collector for CSV files

--- sspi_flask_app/api/core/collect.py
+++ sspi_flask_app/api/core/collect.py
@@ -133,15 +133,24 @@ def gtrans():
 @login_required
 def lfpart():
     def collect_iterator(**kwargs):
-        yield from collectILOData("DF_EAP_DWAP_SEX_AGE_RT", "LFPART", **kwargs)
-    return Response(collect_iterator(Username=current_user.username), mimetype='text/event-stream')
+        yield from collectILOData(
+            "DF_EAP_DWAP_SEX_AGE_RT",
+            "LFPART",
+            QueryParams=".A...AGE_AGGREGATE_Y25-54",
+            **kwargs
+        )
+    return Response(
+        collect_iterator(Username=current_user.username),
+        mimetype='text/event-stream'
+    )
 
 
 @collect_bp.route("/COLBAR")
 @login_required
 def colbar():
     def collect_iterator(**kwargs):
-        yield from collectILOData("DF_ILR_CBCT_NOC_RT", "COLBAR", **kwargs)
+        url_params = ["startPeriod=1990-01-01", "endPeriod=2024-12-31"]
+        yield from collectILOData("DF_ILR_CBCT_NOC_RT", "COLBAR", URLParams=url_params, **kwargs)
     return Response(collect_iterator(Username=current_user.username), mimetype='text/event-stream')
 
 
--- sspi_flask_app/api/datasource/ilo.py
+++ sspi_flask_app/api/datasource/ilo.py
@@ -3,43 +3,26 @@ from sspi_flask_app.models.database import sspi_raw_api_data
 from io import BytesIO
 import zipfile
 
-# def collectILOData(ILOIndicatorCode, IndicatorCode, QueryParams="....", **kwargs):
-#     yield "Sending Data Request to ILO API\n"
-#     response_obj = requests.get(f"https://www.ilo.org/sdmx/rest/data/ILO,{ILOIndicatorCode}/{QueryParams}")
-#     print(str(response_obj.content))
-#     observation = str(response_obj.content)
-#     yield "Data Received from ILO API.  Storing Data in SSPI Raw Data\n"
-#     count = sspi_raw_api_data.raw_insert_one(observation, IndicatorCode, **kwargs)
-#     yield f"Inserted {count} observations into the database."
 
-
-def collectILOData(ILOIndicatorCode, IndicatorCode, **kwargs):
+def collectILOData(ILOIndicatorCode, IndicatorCode, QueryParams="", URLParams=[], **kwargs):
     yield "Sending Data Request to ILO API\n"
-    response_obj = requests.get("https://sdmx.ilo.org/rest/data/ILO,DF_ILR_CBCT_NOC_RT/?format=csv&startPeriod=1990-01-01&endPeriod=2024-12-31")
-    print(response_obj.headers.get('Content-Type'))
+    api_url = f"https://sdmx.ilo.org/rest/data/ILO,{ILOIndicatorCode}"
+    if QueryParams:
+        api_url += f"/{QueryParams}/?format=csv"
+    else:
+        api_url += "/?format=csv"
+    if URLParams:
+        api_url += "&"
+        api_url += "&".join(URLParams)
+    yield "Requesting data from " + api_url
+    response_obj = requests.get(api_url)
     if response_obj.status_code != 200:
         err = f"(HTTP Error {response_obj.status_code})"
-        yield "Failed to fetch data from source" + err
+        yield "\nFailed to fetch data from source" + err
         return
     csv_string = response_obj.content.decode("utf-8")
-    print(csv_string[:500])
     count = sspi_raw_api_data.raw_insert_one(
-                {"csv": csv_string}, IndicatorCode, **kwargs
-        )
-    print("Inserted count:", count)
-    yield f"Inserted {count} observations into the database."
-    yield f"Collection complete for {IndicatorCode} (ILO {ILOIndicatorCode})"
-   # print(str(response_obj.content))
-   # observation = str(response_obj.content)
-   # print("Content-Type:", response_obj.headers.get('Content-Type')) #Content-Type: application/vnd.sdmx.data+json; version=2; charset=utf-8
-   # observation = response_obj.json()
-   # print(type(observation))
-   # response_text = response_obj.content.decode('utf-8')  # Decode bytes to string
-   # observation = json.loads(response_text)  # Parse JSON from the decoded string
-#     yield "Data Received from ILO API.  Storing Data in SSPI Raw Data\n"
-#     count = sspi_raw_api_data.raw_insert_one(observation, IndicatorCode, **kwargs)
-#     print("Inserted count:", count)
-#     yield f"Inserted {count} observations into the database."
-#     retrieved_data = sspi_raw_api_data.find_one({"IndicatorCode": 'COLBAR'})
-#    # print("Retrieved data:", retrieved_data)
-#     print("Type of 'Raw' field:", type(retrieved_data.get("Raw", None)))
+        csv_string, IndicatorCode, **kwargs
+    )
+    yield f"\nInserted {count} observations into the database.\n"
+    yield f"Collection complete for {IndicatorCode} (ILO {ILOIndicatorCode})\n"
