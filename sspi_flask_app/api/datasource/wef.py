0b4ba0c01eb48dc224fa6584d8a06d01341539e8
Frederick Dehmel <dehmelf@berkeley.edu>
2024-12-11 12:05:00 -0800

    change WEF function name

--- sspi_flask_app/api/datasource/wef.py
+++ sspi_flask_app/api/datasource/wef.py
@@ -3,7 +3,7 @@ from io import BytesIO
 import pandas as pd
 from sspi_flask_app.models.database import sspi_raw_api_data
 
-def collectWEFData(SourceIndicatorCode, IndicatorCode, **kwargs):
+def collectWEFdata(SourceIndicatorCode, IndicatorCode, **kwargs):
     url = "https://thedocs.worldbank.org/en/doc/cf8eee7ff5029398f75e897b342e7320-0050122023/related/WEF-GCIHH.xlsx"
     res = requests.get(url)
     if res.status_code != 200:
