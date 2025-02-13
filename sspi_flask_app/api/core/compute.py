46ff04c2fef049c8317f9728756508c0c7abbcad
Tristan Misko <tristanmisko@gmail.com>
2025-02-12 19:23:12 -0500

    fix: Removed Junk File

--- sspi_flask_app/api/core/compute.py
+++ sspi_flask_app/api/core/compute.py
@@ -257,27 +257,12 @@ def compute_stkhlm():
 ### Category: ENERGY ###
 ########################
 
-@compute_bp.route("/NRGINT")
-@login_required
-def compute_nrgint():
-    if not sspi_raw_api_data.raw_data_available("NRGINT"):
-        return redirect(url_for("api_bp.collect_bp.NRGINT"))
-    raw_data = sspi_raw_api_data.fetch_raw_data("NRGINT")
-    intermediate_obs_dict = extract_sdg_pivot_data_to_nested_dictionary(raw_data)
-    computed = flatten_nested_dictionary_nrgint(intermediate_obs_dict)
-    scored_list = score_single_indicator(computed, "NRGINT")
-    clean_document_list, incomplete_observations = filter_incomplete_data(scored_list)
-    sspi_clean_api_data.insert_many(clean_document_list)
-    print(incomplete_observations)
-    return parse_json(clean_document_list)
-
 @compute_bp.route("/COALPW", methods=['GET'])
 @login_required
 def compute_coalpw():
     if not sspi_raw_api_data.raw_data_available("COALPW"):
         return redirect(url_for("api_bp.collect_bp.COALPW"))
     raw_data = sspi_raw_api_data.fetch_raw_data("COALPW")
-
     product_codes = {
         "COAL": "Coal",
         "NATGAS": "Natural gas",
@@ -287,7 +272,7 @@ def compute_coalpw():
         "COMRENEW": "Biofuels and waste",
         "MTOTOIL": "Oil"
     }
-
+    print(product_codes)
     metadata_code_map = {
         "COAL": "TLCOAL",
         "NATGAS": "NATGAS",
@@ -297,7 +282,6 @@ def compute_coalpw():
         "COMRENEW": "BIOWAS",
         "MTOTOIL": "FSLOIL"
     }
-
     intermediate_data = pd.DataFrame(cleanIEAData_altnrg(raw_data, "COALPW"))
     intermediate_data.drop(intermediate_data[intermediate_data["CountryCode"].map(
         lambda s: len(s) != 3)].index, inplace=True)
@@ -339,7 +323,7 @@ def compute_airpol():
     return parse_json(cleaned)
 
 @compute_bp.route("/NRGINT", methods=['GET'])
-# @login_required
+@login_required
 def compute_nrgint():
     if not sspi_raw_api_data.raw_data_available("NRGINT"):
         return redirect(url_for("collect_bp.NRGINT"))
@@ -644,27 +628,6 @@ def compute_intrnt():
         cleaned_list)
     sspi_clean_api_data.insert_many(filtered_list)
     print(incomplete_observations)
-    return parse_json(filtered_list)
-
-
-@compute_bp.route("/FDEPTH", methods=['GET'])
-@login_required
-def compute_fdepth():
-    if not sspi_raw_api_data.raw_data_available("FDEPTH"):
-        return redirect(url_for("collect_bp.FDEPTH"))
-    credit_raw = sspi_raw_api_data.fetch_raw_data(
-        "FDEPTH", IntermediateCode="CREDIT")
-    credit_clean = cleaned_wb_current(credit_raw, "FDEPTH", unit="Percent")
-    deposit_raw = sspi_raw_api_data.fetch_raw_data(
-        "FDEPTH", IntermediateCode="DPOSIT")
-    deposit_clean = cleaned_wb_current(deposit_raw, "FDEPTH", unit="Percent")
-    combined_list = credit_clean + deposit_clean
-    cleaned_list = zip_intermediates(combined_list, "FDEPTH",
-                                     ScoreFunction=lambda CREDIT, DPOSIT: 0.5 * CREDIT + 0.5 * DPOSIT,
-                                     ScoreBy="Score")
-    filtered_list, incomplete_data = filter_incomplete_data(cleaned_list)
-    sspi_clean_api_data.insert_many(filtered_list)
-    print(incomplete_data)
     return parse_json(filtered_list)
 
 
