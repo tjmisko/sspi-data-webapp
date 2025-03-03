abf05e5ba86902415170a7e16267e86afbe0b976
mistyeyee <ritaruotongxu@gmail.com>
2025-03-03 14:12:20 -0800

    final

--- sspi_flask_app/api/datasource/itu.py
+++ sspi_flask_app/api/datasource/itu.py
@@ -13,13 +13,13 @@ def cleanITUData_cybsec(RawData, IndName):
     local_csv_file = pd.read_csv('local/gci-local-indicator-summary.csv')
     columns = ['Country', '2014', 'Rank_2014', '2017', 'Rank_2017', '2018', 'Rank_2018','2020', 'Rank_2020', '2024', 'Rank_2024']
     df = pd.DataFrame(local_csv_file, columns=columns)
-    
+    #initial data manipulation
     df['2024'] = df['2024'] / 100
     df['2020'] = df['2020'] / 100
     df['Country'] = df['Country'].replace('Cape Verde', 'Cabo Verde')
     df['Country'] = df['Country'].replace('Swaziland', 'Eswatini')
 
-
+    #narrow down focus and clean the format
     df_year_only = df.drop(['Rank_2014','Rank_2017', 'Rank_2018', 'Rank_2020', 'Rank_2024'], axis = 1)
     df_melted = df_year_only.melt(id_vars=['Country'], 
                      value_vars=['2014', '2017', '2018', '2020', '2024'], 
@@ -28,7 +28,7 @@ def cleanITUData_cybsec(RawData, IndName):
     
     df_melted['Unit'] = 'Percentage'
     df_melted['IndicatorCode'] = 'CYBSEC'
-    
+    #country with only one year data might not be ideal - no over time comparison.
     df_final = df_melted.dropna()
 
     def get_country_code(country_name):
@@ -38,16 +38,13 @@ def cleanITUData_cybsec(RawData, IndName):
             return pycountry.countries.search_fuzzy(country_name)[0].alpha_3
         except (LookupError, AttributeError, IndexError):
          return None  
-        
+
+    #manually adjust the code
     df_final['CountryCode'] = df_final['Country'].apply(get_country_code)
     df_final['Year'] = df_final['Year'].astype(int)
     df_final.loc[df_final['Country'] == 'Korea (the Republic of)', 'CountryCode'] = 'KOR'
     df_final.loc[df_final['Country'] == 'Democratic Republic of the Congo', 'CountryCode'] = 'COD'
     df_final.loc[df_final['Country'] == 'Niger', 'CountryCode'] = 'NER'
-
-    # missing_country_codes = df_final[df_final['CountryCode'].isnull()]
-    # print("Countries with missing or invalid codes:")
-    # print(missing_country_codes['Country'])
     df_f = df_final.drop('Country', axis = 1)
     return df_f
     
