2ddbb58fed715a08246afa24719746fed16c68df
Tristan Misko <tristanmisko@gmail.com>
2025-03-12 18:27:19 -0700

    feat: Implemented Parsing by Observation

--- sspi_flask_app/api/core/compute/PG/compute_glb.py
+++ sspi_flask_app/api/core/compute/PG/compute_glb.py
@@ -17,22 +17,35 @@ from bs4 import BeautifulSoup
 @compute_bp.route('/FORAID', methods=['GET'])
 @login_required
 def compute_foraid():
-    raw = sspi_raw_api_data.fetch_raw_data("FORAID")[0]
-    raw_data = raw["Raw"]
-    # raw_metadata = raw["Metadata"]
-    soup = BeautifulSoup(raw_data[2:][:-1], "lxml-xml")
-    observations = soup.find_all("Obs")
-    formatted_observations = []
-    for obs in observations:
-        formatted_obs = {}
-        value = obs.find("ObsValue")
-        if value:
-            formatted_obs["Value"] = value.attrs.get("value")
-        for value in obs.find_all("Value"):
-            id = value.attrs.get("id")
-            if id == "TIME_PERIOD":
-                formatted_obs["Year"] = value.attrs.get("value")
-            else:
-                formatted_obs[id] = value.attrs.get("value")
-        formatted_observations.append(formatted_obs)
-    return jsonify(formatted_observations)
+    def parse_observations(xml_string) -> list[dict]:
+        soup = BeautifulSoup(xml_string, "lxml-xml")
+        observations = soup.find_all("Obs")
+        formatted_observations = []
+        for obs in observations:
+            formatted_obs = {}
+            value = obs.find("ObsValue")
+            if value:
+                formatted_obs["Value"] = value.attrs.get("value")
+            for value in obs.find_all("Value"):
+                id = value.attrs.get("id")
+                if id == "TIME_PERIOD":
+                    formatted_obs["Year"] = value.attrs.get("value")
+                else:
+                    formatted_obs[id] = value.attrs.get("value")
+            formatted_observations.append(formatted_obs)
+        return formatted_observations
+
+    def build_metadata_map(metadata_xml):
+        print("String start:", metadata_xml[0:150])
+        print("String end:", metadata_xml[-150:])
+        soup = BeautifulSoup(metadata_xml, "xml")
+        print(soup.prettify())
+        return metadata_xml
+
+    raw = sspi_raw_api_data.fetch_raw_data("FORAID")
+    # raw_data_combined = []
+    # for r in raw:
+    #     obs = parse_observations(r["Raw"][2:][:-1])
+    #     raw_data_combined.extend(obs)
+    metadata_map = build_metadata_map(raw[0]["Raw"][2:][:-1])
+    return parse_json(raw[0])
