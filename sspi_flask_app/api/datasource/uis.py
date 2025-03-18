5ee07048605ec0f3d620ccd297c10317f0987718
max <maxstrongman@berkeley.edu>
2025-03-17 19:33:46 -0700

    wrote UIS clean

--- sspi_flask_app/api/datasource/uis.py
+++ sspi_flask_app/api/datasource/uis.py
@@ -6,10 +6,36 @@ from ..resources.utilities import string_to_float
 
 def collectUISdata(UISIndicatorCode, IndicatorCode, **kwargs):
     yield f"Collecting data for UNESCO Institute for Statistics Indicator {UISIndicatorCode}\n"
-    url_source = f"https://api.uis.unesco.org/api/public/data/indicators?indicator={UISIndicatorCode}]"
+    url_source = f"https://api.uis.unesco.org/api/public/data/indicators?indicator={UISIndicatorCode}"
     response = requests.get(url_source).json()
     count = 0
+    document_list = []
     for obs in response["records"]:
         count += 1
-    sspi_raw_api_data.raw_insert_many(response, IndicatorCode, **kwargs)
+        document_list.append(obs)
+    sspi_raw_api_data.raw_insert_many(document_list, IndicatorCode, **kwargs)
     yield f"Inserted {count} data points; collection complete for UNESCO Institute for Statistics Indicator {UISIndicatorCode}"
+
+def cleanUISdata(raw_data, IndicatorCode, unit, description):
+    clean_data_list = []
+    for obs in raw_data:
+        country = obs["Raw"]["geoUnit"]
+        country_data = countries.get(alpha_3 = country)
+        if not country_data:
+            continue
+        year = int(obs["Raw"]["year"])
+        value = obs["Raw"]["value"]
+        if value == "NaN" or value is None or not value:
+            continue
+        clean_obs = {
+            "CountryCode": country,
+            "IndicatorCode": IndicatorCode,
+            "Description": description,
+            "Year": year,
+            "Unit": unit,
+            "Value": string_to_float(value)
+        }
+        clean_data_list.append(obs)
+    return clean_data_list
+
+        
