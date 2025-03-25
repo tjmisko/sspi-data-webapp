f5cce6f51e8e25ddbc650678ca54184e68ab42f4
Tristan Misko <tristanmisko@gmail.com>
2025-03-25 16:03:04 +0800

    feat: Computed Foreign Aid Totals By Country

--- sspi_flask_app/api/core/compute/PG/compute_glb.py
+++ sspi_flask_app/api/core/compute/PG/compute_glb.py
@@ -1,4 +1,3 @@
-from flask import redirect, url_for, jsonify
 from flask_login import login_required
 from sspi_flask_app.api.core.compute import compute_bp
 from sspi_flask_app.models.database import (
@@ -12,6 +11,7 @@ from sspi_flask_app.api.resources.utilities import (
     score_single_indicator
 )
 from bs4 import BeautifulSoup
+import pycountry
 
 
 @compute_bp.route('/FORAID', methods=['GET'])
@@ -34,17 +34,50 @@ def compute_foraid():
                     formatted_obs[id] = value.attrs.get("value")
             formatted_observations.append(formatted_obs)
         return formatted_observations
-
-    def build_metadata_map(metadata_xml):
-        meta_string = raw[0]["Metadata"][2:][:-1]
-        meta_string = meta_string.replace("\\r\\n", "\n")
-        soup = BeautifulSoup(metadata_xml, "xml")
-        return soup
-
-    raw = sspi_raw_api_data.fetch_raw_data("FORAID")
+    # Metadata was not necessary because all flows were type 206 (ODA)
+    # def build_metadata_map(metadata_xml):
+    #     meta_string = raw[0]["Metadata"][2:][:-1]
+    #     meta_string = meta_string.replace("\\r\\n", "\n")
+    #     soup = BeautifulSoup(metadata_xml, "xml")
+    #     return soup
+    # meta_soup = build_metadata_map(raw[0]["Metadata"])
+    raw = sspi_raw_api_data.fetch_raw_data("FORAID", IntermediateCode="ODAFLW")
     raw_data_combined = []
-    for r in raw:
+    for i, r in enumerate(raw):
+        print(f"Extracting Raw Data for XML Object {i} of {len(raw)}")
         obs = parse_observations(r["Raw"][2:][:-1])
         raw_data_combined.extend(obs)
-    meta_soup = build_metadata_map(raw[0]["Metadata"])
-    return parse_json(raw_data_combined)
+    print("Constructing Tally")
+    aid_dict = {}
+    for obs in raw_data_combined:
+        donor = obs["DONOR"]
+        recipient = obs["RECIPIENT"]
+        year = obs["Year"]
+        donor_year = f"{donor}_{year}"
+        recipient_year = f"{recipient}_{year}"
+        if donor_year not in aid_dict.keys():
+            aid_dict[donor_year] = {"donations": [], "receptions": [], "year": year, "cou": donor}
+        if recipient_year not in aid_dict.keys():
+            aid_dict[recipient_year] = {"donations": [], "receptions": [], "year": year, "cou": recipient}
+        aid_dict[donor_year]["donations"].append(obs)
+        aid_dict[recipient_year]["receptions"].append(obs)
+    print("Computing ODA Totals")
+    aid_totals = []
+    for cou_year, report in aid_dict.items():
+        if not pycountry.countries.get(alpha_3=report["cou"]):
+            continue
+        aid_totals.append({
+            "CountryCode": report["cou"],
+            "Year": report["year"],
+            "TotalDonations": sum([float(d["Value"]) for d in report["donations"] if d.get("Value")]),
+            "TotalReceptions": sum([float(d["Value"]) for d in report["receptions"] if d.get("Value")])
+        })
+    print("Sorting Receivers and Donors")
+    for obs in aid_totals:
+        if obs["TotalDonations"] > obs["TotalReceptions"]:
+            obs["CountryType"] = "Donor"
+        else:
+            obs["CountryType"] = "Recipient"
+
+
+    return parse_json(aid_totals)
