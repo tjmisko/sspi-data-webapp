37552bac52d184e026c972a99e281513a95fa9a6
max <maxstrongman@berkeley.edu>
2025-04-07 14:08:20 -0700

    collect works

--- sspi_flask_app/api/core/collect.py
+++ sspi_flask_app/api/core/collect.py
@@ -460,7 +460,7 @@ def milexp():
 @login_required
 def armexp():
     def collect_iterator(**kwargs):
-        yield from collectSIPRIdata("local/ARMEXP/armexpprocessed.csv", "ARMEXP", **kwargs)
+        yield from collectSIPRIdata("local/ARMEXP/armexp.csv", "ARMEXP", **kwargs)
     return Response(collect_iterator(Username=current_user.username), mimetype='text/event-stream')
 
 #######################################
