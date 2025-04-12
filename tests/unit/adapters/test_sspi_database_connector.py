29dc75073da8b489f55bd7f1ed65ecd0965c908c
Tristan Misko <tristanmisko@gmail.com>
2025-04-12 14:05:06 +0800

    fix: Changed all occurances of LFPART to EMPLOY

--- tests/unit/adapters/test_sspi_database_connector.py
+++ tests/unit/adapters/test_sspi_database_connector.py
@@ -3,4 +3,5 @@ from database_connector import SSPIDatabaseConnector
 
 def test_connector_login():
     connector = SSPIDatabaseConnector()
-    assert connector.token is not None
+    assert connector.local_token is not None
+    assert connector.remote_token is not None
