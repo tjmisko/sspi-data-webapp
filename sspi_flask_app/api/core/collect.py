a7cc42b0f8f971b280aa9033bc909b389fb3a69a
max <maxstrongman@berkeley.edu>
2025-04-11 17:10:17 -0700

    imf api package for tristan

--- sspi_flask_app/api/core/collect.py
+++ sspi_flask_app/api/core/collect.py
@@ -269,6 +269,7 @@ def pubacc():
 # @collect_bp.route("/FSTABL", methods=['GET'])
 # def fstabl():
 #     def collect_iterator(**kwargs):
+# https://github.com/Promptly-Technologies-LLC/imfp --> imf api package
 
 ##########################
 ## Category: INEQUALITY ##
