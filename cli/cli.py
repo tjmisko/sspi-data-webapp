5a16f6f68a01ca625112987d84b1b596c20a8be1
Tristan Misko <tristanmisko@gmail.com>
2025-04-13 16:24:52 +0800

    fix: Cleaned up unnecessary return values and error-handling

--- cli/cli.py
+++ cli/cli.py
@@ -15,14 +15,15 @@ def cli():
 @cli.command(help="Query an SSPI Database")
 @click.argument("database", type=str, required=True)
 @click.argument("indicator_code", type=str, required=False)
-def query(database, indicator_code=None):
+@click.option("--remote", "-r", is_flag=True, help="Send the request to the remote server")
+def query(database, indicator_code=None, remote=False):
     database = full_name(database)
-    session = SSPIDatabaseConnector()
+    connector = SSPIDatabaseConnector()
     request_string = f"/api/v1/query/{database}?"
     if indicator_code:
         indicator_code = indicator_code.upper()
         request_string += f"IndicatorCode={indicator_code}"
-    res = session.get_data_local(request_string)
+    res = connector.call(request_string, remote=remote)
     if res.status_code != 200:
         raise click.ClickException(
             f"Error! Delete Query Failed with Status Code {res.status_code}"
@@ -30,7 +31,6 @@ def query(database, indicator_code=None):
         echo_pretty(res.header)
         echo_pretty(res.text)
     click.echo(json.dumps(res.json()))
-    return res.json()
 
 
 @click.group(help="Delete data from an SSPI Database")
@@ -40,12 +40,23 @@ def delete():
 
 @delete.command()
 @click.argument("database", type=str, required=True)
+@click.option("--remote", "-r", is_flag=True, help="Send the request to the remote server")
+def duplicates(database, remote=False):
+    database = full_name(database)
+    connector = SSPIDatabaseConnector()
+    msg = connector.delete_duplicate_data(database, remote=remote)
+    echo_pretty(msg)
+
+
+@delete.command()
+@click.argument("database", type=str, required=True)
 @click.argument("indicator_code", type=str, required=True)
-def indicator(database, indicator_code):
+@click.option("--remote", "-r", is_flag=True, help="Send the request to the remote server")
+def indicator(database, indicator_code, remote=False):
     database = full_name(database)
     indicator_code = indicator_code.upper()
-    session = SSPIDatabaseConnector()
-    res = session.delete_indicator_data_local(database, indicator_code)
+    connector = SSPIDatabaseConnector()
+    res = connector.delete_indicator_data(database, indicator_code, remote=remote)
     if res.status_code != 200:
         raise click.ClickException(
             f"Error! Delete Request Failed with Status Code {res.status_code}"
@@ -60,20 +71,22 @@ cli.add_command(delete)
 
 @cli.command(help="Collect raw data from source APIs")
 @click.argument("indicator_code", type=str)
-def collect(indicator_code):
-    session = SSPIDatabaseConnector()
+@click.option("--remote", "-r", is_flag=True, help="Send the request to the remote server")
+def collect(indicator_code, remote=False):
+    connector = SSPIDatabaseConnector()
     indicator_code = indicator_code.upper()
-    for msg in session.collect_data_local(indicator_code):
+    for msg in connector.collect(indicator_code, remote=remote):
         echo_pretty(msg)
 
 
 @cli.command(help="Clean raw indicator data and compute indicator scores")
 @click.argument("indicator_code", type=str, required=True)
-def compute(indicator_code):
-    session = SSPIDatabaseConnector()
+@click.option("--remote", "-r", is_flag=True, help="Send the request to the remote server")
+def compute(indicator_code, remote=False):
+    connector = SSPIDatabaseConnector()
     indicator_code = indicator_code.upper()
     request_string = f"/api/v1/compute/{indicator_code}"
-    res = session.get_data_local(request_string)
+    res = connector.call(request_string, remote=remote)
     if res.status_code != 200:
         raise click.ClickException(
             f"Error! Compute Request Failed with Status Code {res.status_code}"
@@ -81,15 +94,15 @@ def compute(indicator_code):
         echo_pretty(res.header)
         echo_pretty(res.text)
     click.echo(json.dumps(res.json()))
-    return res.json()
 
 
 @cli.group(invoke_without_command=True, help="Finalize clean data for plotting")
+@click.option("--remote", "-r", is_flag=True, help="Send the request to the remote server")
 @click.pass_context
-def finalize(ctx):
+def finalize(ctx, remote=False):
     if ctx.invoked_subcommand is None:
-        session = SSPIDatabaseConnector()
-        for msg in session.finalize_data_local():
+        connector = SSPIDatabaseConnector()
+        for msg in connector.finalize(remote=remote):
             echo_pretty(msg)
 
 
@@ -99,10 +112,11 @@ def dynamic():
 
 
 @finalize.command(help="Finalize indicator dynamic line data")
-def line():
+@click.option("--remote", "-r", is_flag=True, help="Send the request to the remote server")
+def line(remote=False):
     click.echo("Finalizing Dynamic Line Data")
-    session = SSPIDatabaseConnector()
-    res = session.call_local("/api/v1/production/finalize/dynamic/line")
+    connector = SSPIDatabaseConnector()
+    res = connector.call("/api/v1/production/finalize/dynamic/line", remote=remote)
     if res.status_code != 200:
         raise click.ClickException(
             f"Error! Finalize Request Failed with Status Code {res.status_code}"
@@ -117,5 +131,69 @@ finalize.add_command(dynamic)
 cli.add_command(finalize)
 
 
+@cli.command(help="Pull local data to remote server")
+@click.argument("database", type=str, required=True)
+@click.argument("indicator_code", type=str, required=True)
+def pull(database: str, indicator_code: str):
+    database = full_name(database)
+    indicator_code = indicator_code.upper()
+    confirm_msg_lst = [
+        "Confirm ",
+        click.style("PULL", fg="yellow"),
+        " of all observations of ",
+        click.style(indicator_code, fg="yellow"),
+        " from ",
+        click.style("Remote", fg="yellow"),
+        " database ",
+        click.style(database, fg="yellow")
+    ]
+    if click.confirm("".join(confirm_msg_lst)):
+        connector = SSPIDatabaseConnector()
+        res = connector.call(
+            f"/api/v1/pull/{database}/{indicator_code}",
+            method="POST"
+        )
+        if res.status_code != 200:
+            raise click.ClickException(
+                f"Error! Finalize Request Failed with Status Code {res.status_code}"
+            )
+            echo_pretty(res.header)
+            echo_pretty(res.text)
+        echo_pretty(res.text)
+
+
+@cli.command(help="Push local data to remote server")
+@click.argument("database", type=str, required=True)
+@click.argument("indicator_code", type=str, required=True)
+def push(database: str, indicator_code: str):
+    database = full_name(database)
+    indicator_code = indicator_code.upper()
+    confirm_msg_lst = [
+        "Confirm ",
+        click.style("PUSH", fg="yellow"),
+        " of all observations of ",
+        click.style(indicator_code, fg="yellow"),
+        " from ",
+        click.style("Local", fg="yellow"),
+        " database ",
+        click.style(database, fg="yellow")
+    ]
+    if click.confirm("".join(confirm_msg_lst)):
+        connector = SSPIDatabaseConnector()
+        res = connector.call(
+            f"/api/v1/push/{database}/{indicator_code}",
+            method="POST"
+        )
+        if res.status_code != 200:
+            raise click.ClickException(
+                f"Error! Finalize Request Failed with Status Code {res.status_code}"
+            )
+            echo_pretty(res.header)
+            echo_pretty(res.text)
+        echo_pretty(res.text)
+
+
 if __name__ == "__main__":
     cli()
+
+
