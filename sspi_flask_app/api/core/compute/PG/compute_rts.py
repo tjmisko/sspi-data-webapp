cd903120fe4034f829755e319b0dceff94d4db3f
Tristan Misko <tristanmisko@gmail.com>
2025-04-14 21:44:53 +0800

    fix: Simplified /api/v1/compute/EDEMOC

--- sspi_flask_app/api/core/compute/PG/compute_rts.py
+++ sspi_flask_app/api/core/compute/PG/compute_rts.py
@@ -57,7 +57,27 @@ def compute_edemoc():
     app.logger.info("Running /api/v1/compute/EDEMOC")
     sspi_clean_api_data.delete_many({"IndicatorCode": "EDEMOC"})
     raw_data = sspi_raw_api_data.fetch_raw_data("EDEMOC")
-    cleaned_list = cleanEDEMOCdata(raw_data)
-    scored_list = score_single_indicator(cleaned_list, "EDEMOC")
+    fragments = []
+    for obs in raw_data:
+        fragments.append((obs["FragmentNumber"], obs["Raw"]["csv_fragment"]))
+    fragments.sort(key=lambda x: x[0])
+    full_csv = "".join(fragment for _, fragment in fragments)
+    df = pd.read_csv(StringIO(full_csv))
+    filtered_df = df[['country_text_id', 'year', 'v2x_polyarchy']]
+    current_year = datetime.now().year
+    filtered_df = filtered_df[(filtered_df["year"] > 1990) & (filtered_df["year"] < current_year)]
+    obs_list = json.loads(str(filtered_df.to_json(orient='records')))
+    clean_list = []
+    for obs in obs_list:
+        if obs["v2x_polyarchy"] is None:
+            continue
+        clean_list.append({
+            "IndicatorCode": "EDEMOC",
+            "CountryCode": obs["country_text_id"],
+            "Year": obs["year"],
+            "Value": obs["v2x_polyarchy"],
+            "Unit": "Index"
+        })
+    scored_list = score_single_indicator(clean_list, "EDEMOC")
     sspi_clean_api_data.insert_many(scored_list)
     return parse_json(scored_list)
