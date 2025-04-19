5a5f9d48bef9cdbe323498217507e6d79a9c639d
Tristan Misko <tristanmisko@gmail.com>
2025-04-19 15:29:45 +0800

    feat: Implemented /api/v1/collect/BEEFMK

--- sspi_flask_app/api/datasource/fao.py
+++ sspi_flask_app/api/datasource/fao.py
@@ -2,13 +2,14 @@ import requests
 from sspi_flask_app.models.database import sspi_raw_api_data
 
 
-def collectUNFAOData(UNFAO_element: str, UNFAO_item: str, IndicatorCode: str, **kwargs):
+def collectUNFAOData(UNFAO_element: str, UNFAO_item: str, UNFAO_domain: str, IndicatorCode: str, **kwargs):
     yield "Collecting UNFAO data\n"
-    base_url = "https://faostatservices.fao.org/api/v1/en/data/RL?"
+    base_url = f"https://faostatservices.fao.org/api/v1/en/data/{UNFAO_domain}?"
     element = f"&element={UNFAO_element}"
     item = f"&item={UNFAO_item}"
     default_options = "&area_cs=ISO3&show_codes=true&show_unit=true&show_flags=true"
     default_options += "&show_notes=true&null_values=false&output_type=objects"
+    print(f"{base_url}{element}{item}{default_options}")
     res = requests.get(f"{base_url}{element}{item}{default_options}")
     raw_data = res.json()
     sspi_raw_api_data.raw_insert_one(raw_data, IndicatorCode, **kwargs)
