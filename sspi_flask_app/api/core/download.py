a80d55260a4c98288d5de2ff0b231442c8cbe640
Tristan Misko <tristanmisko@gmail.com>
2025-04-29 11:41:38 +0800

    fix: Cleaned Up Old Download Form Logic

--- sspi_flask_app/api/core/download.py
+++ sspi_flask_app/api/core/download.py
@@ -3,56 +3,79 @@ from flask import Blueprint, request, send_file
 import pandas as pd
 from ..resources.utilities import lookup_database, parse_json
 from flask_wtf import FlaskForm
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import SubmitField, SelectField
 from wtforms.validators import (
-    DataRequired,
-    InputRequired,
-    Length,
-    ValidationError
+    DataRequired
 )
 from sspi_flask_app.models.database import sspidb, sspi_metadata
 
 
 download_bp = Blueprint("download_bp", __name__,
-                        template_folder="templates", 
-                        static_folder="static", 
+                        template_folder="templates",
+                        static_folder="static",
                         url_prefix="/download")
 
 db_choices = sspidb.list_collection_names()
 ic_choices = sspi_metadata.indicator_codes()
 cg_choices = sspi_metadata.country_groups()
 
+
 class ClientDownloadForm(FlaskForm):
-    database = SelectField(choices = [("sspi_main_data_v3", "SSPI V3 Data (2018 Only)"), ("sspi_final_api_data", "SSPI Dynamic Data (Experimental)")], validators=[DataRequired()], default="sspi_main_data_v3", label="Database")
-    indicator_code = SelectField(choices = ic_choices, validators=[DataRequired()], label="Indicator Code", render_kw={"placeholder": "Indicator Code"})
-    country_group = SelectField(choices = cg_choices, validators=[DataRequired()], default="sspi49", label="Country Group")
+    database = SelectField(
+        choices=[
+            ("sspi_main_data_v3", "SSPI V3 Data (2018 Only)"),
+            ("sspi_final_api_data", "SSPI Dynamic Data (Experimental)")
+        ],
+        validators=[DataRequired()],
+        default="sspi_main_data_v3",
+        label="Database"
+    )
+    indicator_code = SelectField(
+        choices=ic_choices,
+        validators=[DataRequired()],
+        label="Indicator Code",
+        render_kw={"placeholder": "Indicator Code"}
+    )
+    country_group = SelectField(
+        choices=cg_choices,
+        validators=[DataRequired()],
+        default="SSPI49",
+        label="Country Group"
+    )
     submit = SubmitField('Download Data')
+
 
 @download_bp.route("/client", methods=['POST'])
 def client_download():
     return "data"
 
-  
+
 def fetch_data_for_download(request_args):
     """
     request_args has type ImmutableMultiDict
     """
-    MongoQuery = {}
+    mongo_query = {}
     if request_args.getlist('IndicatorCode'):
-          MongoQuery["IndicatorCode"] = {"$in": request.args.getlist('IndicatorCode')}
+        mongo_query["IndicatorCode"] = {
+            "$in": request.args.getlist('IndicatorCode')}
     if request_args.get('CountryGroup'):
-        MongoQuery['CountryCode'] = {"$in": country_group(request.args.get('CountryGroup'))}
+        mongo_query['CountryCode'] = {
+            "$in": sspi_metadata.country_group(request.args.get('CountryGroup'))
+        }
     elif request_args.getlist('CountryCode'):
-        MongoQuery["CountryCode"] = {"$in": request.args.getlist('CountryCode')}
+        mongo_query["CountryCode"] = {
+            "$in": request.args.getlist('CountryCode')
+        }
     if request_args.getlist('YEAR'):
-        MongoQuery["timePeriod"] = {"$in": request.args.getlist('timePeriod')}
-    database_name = request_args.get("database", default = "sspi_main_data_v3")
+        mongo_query["timePeriod"] = {
+            "$in": request.args.getlist('timePeriod')
+        }
+    database_name = request_args.get("database", default="sspi_main_data_v3")
     dataframe = lookup_database(database_name)
-    data_to_download = parse_json(dataframe.find(MongoQuery))
-    print(MongoQuery)
+    data_to_download = parse_json(dataframe.find(mongo_query))
+    print(mongo_query)
     return data_to_download
-  
+
 
 @download_bp.route("/csv")
 def download_csv():
@@ -64,10 +87,13 @@ def download_csv():
     mem = BytesIO()
     mem.write(df.encode('utf-8'))
     mem.seek(0)
-    return send_file(mem,
-                     mimetype='text/csv',
-                     download_name='data.csv',
-                     as_attachment=True)
+    return send_file(
+        mem,
+        mimetype='text/csv',
+        download_name='SSPIData.csv',
+        as_attachment=True
+    )
+
 
 @download_bp.route("/json")
 def download_json():
