5b3ed5f3c8f5e17d1b2bd01a08c9beae733e9ff9
Tristan Misko <tristanmisko@gmail.com>
2025-06-17 17:41:42 +0900

    feat: Implemented /api/v1/impute/ENRPRI

--- sspi_flask_app/api/core/sspi/pg/edu/enrpri.py
+++ sspi_flask_app/api/core/sspi/pg/edu/enrpri.py
@@ -1,19 +1,27 @@
-from flask import Response, current_app as app
-from sspi_flask_app.api.core.sspi import collect_bp
-from sspi_flask_app.api.core.sspi import compute_bp
-from sspi_flask_app.models.database import (
-    sspi_raw_api_data,
-    sspi_clean_api_data
-)
+from flask import Response
+from flask import current_app as app
+from flask_login import current_user, login_required
+
+from sspi_flask_app.api.core.sspi import collect_bp, compute_bp, impute_bp
 from sspi_flask_app.api.datasource.uis import (
-    collectUISdata,
     cleanUISdata,
+    collectUISdata,
 )
-from flask_login import login_required, current_user
 from sspi_flask_app.api.resources.utilities import (
+    extrapolate_backward,
+    extrapolate_forward,
+    impute_global_average,
+    interpolate_linear,
     parse_json,
-    score_single_indicator
+    score_single_indicator,
 )
+from sspi_flask_app.models.database import (
+    sspi_clean_api_data,
+    sspi_imputed_data,
+    sspi_metadata,
+    sspi_raw_api_data,
+)
+
 
 @collect_bp.route("/ENRPRI", methods=['GET'])
 @login_required
@@ -34,3 +42,19 @@ def compute_enrpri():
     scored_list = score_single_indicator(cleaned_list, "ENRPRI")
     sspi_clean_api_data.insert_many(scored_list)
     return parse_json(scored_list)
+
+
+@impute_bp.route("/ENRPRI", methods=['POST'])
+@login_required
+def impute_enrpri():
+    app.logger.info("Running /api/v1/impute/ENRPRI")
+    sspi_imputed_data.delete_many({"IndicatorCode": "ENRPRI"})
+    clean_enrpri = sspi_clean_api_data.find({"IndicatorCode": "ENRPRI"})
+    forward = extrapolate_forward(clean_enrpri, 2023, impute_only=True)
+    backward = extrapolate_backward(clean_enrpri, 2000, impute_only=True)
+    interpolated = interpolate_linear(clean_enrpri, impute_only=True)
+    imputed_enrpri = forward + backward + interpolated
+    # Handle China, which is missing all observations
+    chn_enrpri = impute_global_average("CHN", 2000, 2023, "Indicator", "ENRPRI", clean_enrpri)
+    sspi_imputed_data.insert_many(imputed_enrpri + chn_enrpri)
+    return parse_json(imputed_enrpri + chn_enrpri)
