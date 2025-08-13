2dbfa455723f2b1d7359acad41fa31a966817a2e
Tristan Misko <tristanmisko@gmail.com>
2025-08-13 12:05:53 -0700

    fix: WID_BFIFTY dataset_cleaner boilerplate

--- sspi_flask_app/api/core/datasets/wid_bfifty.py
+++ sspi_flask_app/api/core/datasets/wid_bfifty.py
@@ -1,4 +1,4 @@
-from sspi_flask_app.api.datasource.wid import collect_wid_data
+from sspi_flask_app.api.datasource.wid import collect_wid_data, fetch_wid_raw_data, filter_wid_csv
 from sspi_flask_app.api.core.datasets import dataset_collector, dataset_cleaner
 from sspi_flask_app.models.database import sspi_raw_api_data, sspi_clean_api_data, sspi_metadata
 from sspi_flask_app.api.resources.utilities import parse_json
@@ -10,12 +10,17 @@ def collect_wid_bfifty(**kwargs):
 
 @dataset_cleaner("WID_BFIFTY")
 def clean_wid_bfifty():
+    """
+    Clean the WID Dataset for the bottom 50% income share
+    """
     sspi_clean_api_data.delete_many({"DatasetCode": "WID_BFIFTY"})
-    source_info = sspi_metadata.get_source_info("WID_BFIFTY")
-    raw_data = sspi_raw_api_data.fetch_raw_data(source_info)
-    # TODO: Implement cleaning logic for WID_BFIFTY
-    # This will be implemented in the next task
+    sspi_67 = sspi_metadata.country_group("SSPI67")
     cleaned_data = []
+    year_range = list(range(2000, 2025))
+    for cou in sspi_67:
+        raw, meta = fetch_wid_raw_data(cou)
+        output_list = filter_wid_csv("WID_BFIFTY", raw["Raw"], cou, "p0p50", "sptincj992", year_range, metadata_csv_string=meta["Raw"])
+        cleaned_data.extend(output_list)
     sspi_clean_api_data.insert_many(cleaned_data)
     sspi_metadata.record_dataset_range(cleaned_data, "WID_BFIFTY")
     return parse_json(cleaned_data)
