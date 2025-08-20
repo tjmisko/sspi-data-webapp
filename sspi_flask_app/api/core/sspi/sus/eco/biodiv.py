bc270148c2bfc16c5cc2322ee1b4e8aeca518ae3
Tristan Misko <tristanmisko@gmail.com>
2025-08-20 07:04:00 -0700

    style: BIODIV

--- sspi_flask_app/api/core/sspi/sus/eco/biodiv.py
+++ sspi_flask_app/api/core/sspi/sus/eco/biodiv.py
@@ -19,34 +19,6 @@ from sspi_flask_app.models.database import (
 log = logging.getLogger(__name__)
 
 
-# def compute_biodiv_old():
-#     app.logger.info("Running /api/v1/compute/BIODIV")
-#     sspi_clean_api_data.delete_many({"IndicatorCode": "BIODIV"})
-#     sspi_incomplete_indicator_data.delete_many({"IndicatorCode": "BIODIV"})
-#     raw_data = sspi_raw_api_data.fetch_raw_data("BIODIV")
-#     extracted_biodiv = extract_sdg(raw_data)
-#     idcode_map = {
-#         "ER_MRN_MPA": "MARINE",
-#         "ER_PTD_TERR": "TERRST",
-#         "ER_PTD_FRHWTR": "FRSHWT",
-#     }
-#     rename_map = {"units": "Unit", "seriesDescription": "Description"}
-#     drop_list = [
-#         "goal",
-#         "indicator",
-#         "series",
-#         "seriesCount",
-#         "target",
-#         "geoAreaCode",
-#         "geoAreaName",
-#     ]
-#     intermediate_list = filter_sdg(
-#         extracted_biodiv,
-#         idcode_map,
-#         rename_map,
-#         drop_list,
-#     )
-
 @compute_bp.route("/BIODIV", methods=["POST"])
 @login_required
 def compute_biodiv():
