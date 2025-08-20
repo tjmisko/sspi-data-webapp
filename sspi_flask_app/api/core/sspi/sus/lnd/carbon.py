458f485dec278fa82f43b9893d381346658acf76
Tristan Misko <tristanmisko@gmail.com>
2025-08-20 11:39:12 -0700

    fix: Imputations for CARBON

--- sspi_flask_app/api/core/sspi/sus/lnd/carbon.py
+++ sspi_flask_app/api/core/sspi/sus/lnd/carbon.py
@@ -1,16 +1,19 @@
-from sspi_flask_app.api.core.sspi import compute_bp
+from sspi_flask_app.api.core.sspi import compute_bp, impute_bp
 from flask import current_app as app
 from flask_login import login_required
 from sspi_flask_app.models.database import (
     sspi_clean_api_data,
     sspi_indicator_data,
     sspi_incomplete_indicator_data,
+    sspi_imputed_data,
     sspi_metadata
 )
 from sspi_flask_app.api.resources.utilities import (
     parse_json,
     goalpost,
-    score_indicator
+    score_indicator,
+    extrapolate_forward,
+    impute_reference_class_average
 )
 
 
@@ -46,3 +49,35 @@ def compute_carbon():
     sspi_indicator_data.insert_many(clean_list)
     sspi_incomplete_indicator_data.insert_many(incomplete_list)
     return parse_json(clean_list)
+
+
+@impute_bp.route("/CARBON", methods=["POST"])
+@login_required
+def impute_carbon():
+    mongo_query = {"IndicatorCode": "CARBON"}
+    sspi_imputed_data.delete_many(mongo_query)
+    
+    # Get complete indicator data
+    clean_list = sspi_indicator_data.find(mongo_query)
+    
+    # Extrapolate indicator scores forward to 2023
+    imputed_carbon = extrapolate_forward(
+        clean_list, 2023, series_id=["CountryCode", "IndicatorCode"], impute_only=True
+    )
+    
+    # Handle countries with no data using reference class average
+    countries_no_data = ["BEL", "ARE", "LUX", "KWT"]
+    ref_data = [d for d in clean_list if d["CountryCode"] not in countries_no_data]
+    
+    for country in countries_no_data:
+        if ref_data:
+            country_imputed = impute_reference_class_average(
+                country, 2000, 2023, "Indicator", "CARBON", ref_data
+            )
+            imputed_carbon.extend(country_imputed)
+    
+    # Insert into database
+    if imputed_carbon:
+        sspi_imputed_data.insert_many(imputed_carbon)
+    
+    return parse_json(imputed_carbon)
