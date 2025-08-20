30ad5677be622adeeeac1f2f49b9ade65aa7449c
Tristan Misko <tristanmisko@gmail.com>
2025-08-20 07:03:29 -0700

    feat: Collect, Clean, and Computed MSWGEN Implemented

--- sspi_flask_app/api/core/sspi/sus/wst/mswgen.py
+++ sspi_flask_app/api/core/sspi/sus/wst/mswgen.py
@@ -1,2 +1,34 @@
-from flask_login import login_required, current_user
+import logging
+
+from flask import Response
+from flask import current_app as app
+from flask_login import current_user, login_required
+
+from sspi_flask_app.api.core.sspi import compute_bp
+from sspi_flask_app.api.datasource.unsdg import (
+    collect_sdg_indicator_data,
+    extract_sdg,
+    filter_sdg,
+)
+from sspi_flask_app.api.resources.utilities import goalpost, parse_json, score_indicator
+from sspi_flask_app.models.database import (
+    sspi_clean_api_data,
+    sspi_indicator_data,
+    sspi_metadata,
+    sspi_raw_api_data,
+)
+
+@compute_bp.route("/MSWGEN", methods=['POST'])
+@login_required
+def compute_mswgen():
+    sspi_indicator_data.delete_many({"IndicatorCode": "MSWGEN"})
+    dataset_list = sspi_clean_api_data.find({"DatasetCode": "EPI_MSWGEN"})
+    lg, ug = sspi_metadata.get_goalposts("MSWGEN") 
+    scored_data, _ = score_indicator(
+        dataset_list, "MSWGEN",
+        score_function=lambda EPI_MSWGEN: goalpost(EPI_MSWGEN, lg, ug),
+        unit="Index"
+    )
+    sspi_indicator_data.insert_many(scored_data)
+    return parse_json(scored_data)
 
