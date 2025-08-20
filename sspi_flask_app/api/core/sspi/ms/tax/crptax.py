a490ff1cb2adc846daf4be3c455015e6678555d9
Tristan Misko <tristanmisko@gmail.com>
2025-08-20 10:21:33 -0700

    fix: CRPTAX now Collected

--- sspi_flask_app/api/core/sspi/ms/tax/crptax.py
+++ sspi_flask_app/api/core/sspi/ms/tax/crptax.py
@@ -3,10 +3,6 @@ from flask import current_app as app
 from flask_login import current_user, login_required
 
 from sspi_flask_app.api.core.sspi import compute_bp, impute_bp
-from sspi_flask_app.api.datasource.taxfoundation import (
-    clean_tax_foundation,
-    collect_tax_foundation_data,
-)
 from sspi_flask_app.api.resources.utilities import (
     extrapolate_backward,
     interpolate_linear,
@@ -16,34 +12,25 @@ from sspi_flask_app.api.resources.utilities import (
 )
 from sspi_flask_app.models.database import (
     sspi_clean_api_data,
+    sspi_indicator_data,
     sspi_imputed_data,
-    sspi_raw_api_data,
     sspi_metadata
 )
-
-
-# @collect_bp.route("/CRPTAX", methods=['GET'])
-# @login_required
-# def crptax():
-#     def collect_iterator(**kwargs):
-#         yield from collect_tax_foundation_data('CRPTAX', **kwargs)
-#     return Response(collect_iterator(Username=current_user.username), mimetype='text/event-stream')
 
 
 @compute_bp.route("/CRPTAX", methods=["POST"])
 @login_required
 def compute_crptax():
     app.logger.info("Running /api/v1/compute/CRPTAX")
-    sspi_clean_api_data.delete_many({"IndicatorCode": "CRPTAX"})
-    crptax_raw = sspi_raw_api_data.fetch_raw_data("CRPTAX")
-    crptax_clean = clean_tax_foundation(crptax_raw, "CRPTAX", "Tax Rate", "Corporate Taxes")
+    sspi_indicator_data.delete_many({"IndicatorCode": "CRPTAX"})
+    crptax_clean = sspi_clean_api_data.find({"DatasetCode": "TF_CRPTAX"})
     lg, ug = sspi_metadata.get_goalposts("CRPTAX")
     scored_list, _ = score_indicator(
         crptax_clean, "CRPTAX",
         score_function=lambda TF_CRPTAX: goalpost(TF_CRPTAX, lg, ug),
         unit="Tax Rate"
     )
-    sspi_clean_api_data.insert_many(scored_list)
+    sspi_indicator_data.insert_many(scored_list)
     return parse_json(scored_list)
 
 
@@ -52,7 +39,7 @@ def compute_crptax():
 def impute_crptax():
     app.logger.info("Running /api/v1/impute/CRPTAX")
     sspi_imputed_data.delete_many({"IndicatorCode": "CRPTAX"})
-    clean_crptax = sspi_clean_api_data.find({"IndicatorCode": "CRPTAX"})
+    clean_crptax = sspi_indicator_data.find({"IndicatorCode": "CRPTAX"})
     # forward = extrapolate_forward(clean_crptax, 2023, impute_only=True)
     backward = extrapolate_backward(clean_crptax, 2000, impute_only=True)
     interpolated = interpolate_linear(clean_crptax, impute_only=True)
