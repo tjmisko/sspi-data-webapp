30c834e32757df2de5f853ba9950e371980e6e1a
Tristan Misko <tristanmisko@gmail.com>
2025-11-13 14:22:35 -0800

    fix: Removed Old API Blueprint Junk

--- sspi_flask_app/api/api.py
+++ sspi_flask_app/api/api.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint
 
 api_bp = Blueprint(
     'api_bp', __name__,
@@ -7,8 +6,3 @@ api_bp = Blueprint(
     static_folder='static',
     url_prefix='/api/v1'
 )
-
-@api_bp.route("/", methods=["GET"])
-@login_required
-def api_dashboard():
-    return render_template("internal-dashboard.html")
